7515f7d004950f0c9ce83fcd4952959688841619
gkbrown <dev-null@apache.org>
2008-06-13 21:03:11 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@753671 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tutorials/src/pivot/tutorials/layout/StackPanes.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2008 VMware, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pivot.tutorials.layout;
+
+import pivot.wtk.Application;
+import pivot.wtk.Component;
+import pivot.wtk.Display;
+import pivot.wtk.Window;
+import pivot.wtkx.ComponentLoader;
+
+public class StackPanes implements Application {
+    private Window window = null;
+
+    public void startup() throws Exception {
+        ComponentLoader.initialize();
+        ComponentLoader componentLoader = new ComponentLoader();
+
+        Component content =
+            componentLoader.load("pivot/tutorials/layout/stackpanes.wtkx");
+
+        window = new Window();
+        window.setContent(content);
+        window.getAttributes().put(Display.MAXIMIZED_ATTRIBUTE,
+            Boolean.TRUE);
+        window.open();
+    }
+
+    public void shutdown() throws Exception {
+        window.close();
+    }
+
+    public void suspend() throws Exception {
+    }
+
+    public void resume() throws Exception {
+    }
+}
