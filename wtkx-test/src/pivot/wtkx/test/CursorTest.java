9563978c283ef969788a1172dfd4d7023d5f42f7
gkbrown <dev-null@apache.org>
2008-06-16 15:34:28 +0000

    Initial import.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@753694 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wtkx-test/src/pivot/wtkx/test/CursorTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2008 VMware, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pivot.wtkx.test;
+
+import pivot.wtk.Application;
+import pivot.wtk.Component;
+import pivot.wtk.Window;
+import pivot.wtkx.ComponentLoader;
+
+public class CursorTest implements Application {
+    private Window window = null;
+
+    public void startup() throws Exception {
+        ComponentLoader.initialize();
+        ComponentLoader componentLoader = new ComponentLoader();
+
+        Component content = componentLoader.load("pivot/wtkx/test/cursor.wtkx");
+
+        window = new Window();
+        window.setTitle("Cursor Test");
+        window.setContent(content);
+
+        window.setPreferredSize(320, 240);
+        window.open();
+    }
+
+    public void shutdown() throws Exception {
+        window.close();
+    }
+
+    public void suspend() throws Exception {
+    }
+
+    public void resume() throws Exception {
+    }
+}
