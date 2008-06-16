84933c7a9e3361efb94e6a79e68160f103b93877
gkbrown <dev-null@apache.org>
2008-06-16 15:30:21 +0000

    Initial import.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@753688 13f79535-47bb-0310-9956-ffa450edef68

---
+++ web-test-server/src/pivot/web/test/server/WebQueryTestServlet.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2008 VMware, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pivot.web.test.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import pivot.serialization.JSONSerializer;
+import pivot.serialization.SerializationException;
+import sun.misc.BASE64Decoder;
+
+public class WebQueryTestServlet extends HttpServlet {
+    public static final long serialVersionUID = 0;
+
+    private String username = null;
+    private static final String BASIC_AUTHENTICATION_TAG = "Basic";
+
+    @Override
+    public void init(ServletConfig config) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null) {
+            response.setHeader("WWW-Authenticate", "BASIC realm=\""
+                + request.getServletPath() +"\"");
+            response.setStatus(401);
+            response.setContentLength(0);
+            response.flushBuffer();
+        } else {
+            String encodedCredentials = authorization.substring(BASIC_AUTHENTICATION_TAG.length() + 1);
+
+            // TODO Use a different decoder; sun.misc.BASE64Decoder is undocumented
+            BASE64Decoder decoder = new BASE64Decoder();
+            String decodedCredentials = new String(decoder.decodeBuffer(encodedCredentials));
+            String[] credentials = decodedCredentials.split(":");
+            username = credentials[0];
+
+            // TODO Parse query string
+
+            super.service(request, response);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        String pathInfo = request.getPathInfo();
+        String queryString = request.getQueryString();
+
+        response.setStatus(200);
+        response.setContentType("application/json;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("\"User " + username + " is authorized to access "
+            + pathInfo + "?" + queryString + "\"");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        ServletInputStream inputStream = request.getInputStream();
+        JSONSerializer jsonSerializer = new JSONSerializer();
+
+        try {
+            Object value = jsonSerializer.readObject(inputStream);
+            System.out.println(value);
+            response.setStatus(201);
+            response.setHeader("Location", request.getPathInfo() +"#101");
+        } catch(SerializationException exception) {
+            System.out.println(exception);
+            response.setStatus(400);
+        }
+
+        response.setContentLength(0);
+        response.flushBuffer();
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        ServletInputStream inputStream = request.getInputStream();
+        JSONSerializer jsonSerializer = new JSONSerializer();
+
+        try {
+            Object value = jsonSerializer.readObject(inputStream);
+            System.out.println(value);
+            response.setStatus(200);
+        } catch(SerializationException exception) {
+            System.out.println(exception);
+            response.setStatus(400);
+        }
+
+        response.setContentLength(0);
+        response.flushBuffer();
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        response.setStatus(200);
+        response.setContentLength(0);
+        response.flushBuffer();
+    }
+}
