28ef9f4f99a577b659f258e53ae468d8b1fb19fc
gkbrown <dev-null@apache.org>
2008-07-10 18:44:16 +0000

    Delete theme JARs in wtk-clean target
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@753728 13f79535-47bb-0310-9956-ffa450edef68

--- wtk/src/pivot/wtk/Theme.java
+++ wtk/src/pivot/wtk/Theme.java
@@ -6,6 +6,10 @@ public abstract class Theme {
     public abstract Class<? extends Skin> getSkinClass(Class<? extends Component> componentClass);
 
     public static Theme getTheme() {
+        if (theme == null) {
+            throw new IllegalStateException("No installed theme.");            
+        }
+        
         return theme;
     }
 
