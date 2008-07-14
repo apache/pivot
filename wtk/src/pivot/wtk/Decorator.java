7dd7741d4c086cb4d836c846e71006f910b420a0
gkbrown <dev-null@apache.org>
2008-07-14 21:58:18 +0000

    Add Decorator interface.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@753733 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wtk/src/pivot/wtk/Decorator.java
@@ -0,0 +1,8 @@
+package pivot.wtk;
+
+import java.awt.Graphics2D;
+
+public interface Decorator {
+    public Graphics2D prepare(Component component, Graphics2D graphics);
+    public void update();
+}
