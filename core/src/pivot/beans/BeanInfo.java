033ea963e2c04fe7118fee7c8100fe1cff46389f
eryzhikov <dev-null@apache.org>
2008-07-28 23:51:39 +0000

    Added retention policy and inherited
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@753760 13f79535-47bb-0310-9956-ffa450edef68

--- core/src/pivot/beans/BeanInfo.java
+++ core/src/pivot/beans/BeanInfo.java
@@ -1,8 +1,14 @@
 package pivot.beans;
 
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * TODO
  */
+@Inherited 
+@Retention(RetentionPolicy.RUNTIME)
 public @interface BeanInfo {
     String icon();
 }
