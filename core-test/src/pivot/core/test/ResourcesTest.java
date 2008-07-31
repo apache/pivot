0351eaa7c73718ea8a155ee8467c6b2aff21b7f0
Christopher Brind <brindy@apache.org>
2008-07-31 06:58:32 +0000

    added unit library to .classpath added ResourcesTest to exercise Resources added sample JSON files for testing implemented Resources
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@753780 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core-test/src/pivot/core/test/ResourcesTest.java
@@ -0,0 +1,128 @@
+package pivot.core.test;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import pivot.collections.List;
+import pivot.collections.Map;
+import pivot.serialization.SerializationException;
+import pivot.util.Resources;
+
+public class ResourcesTest extends TestCase {
+
+	public void testReadDefaultLocale() throws Exception {
+
+		Resources res = new Resources("resources/test1.json");
+		assertResources(res, "SGML", "Standard Generalized Markup Language");
+	}
+
+	/**
+	 * The resource overrides the term for the country.
+	 * 
+	 * @throws Exception
+	 */
+	public void testRead_GB_Locale() throws Exception {
+		Resources res = new Resources("resources/test2.json");
+		assertResources(res, "SGML",
+				"How Do, Youth, Standard Generalized Markup Language");
+
+	}
+
+	/**
+	 * The resource overrides the term for the country and the acronym for the
+	 * language.
+	 * 
+	 * @throws Exception
+	 */
+	public void testRead_GB_en_Locale() throws Exception {
+		Resources res = new Resources("resources/test3.json");
+		assertResources(res, "XSGML",
+				"How Do, Youth, Standard Generalized Markup Language");
+
+	}
+
+	/**
+	 * The resource overrides the term and the acronym for the country.
+	 * 
+	 * @throws Exception
+	 */
+	public void testRead_GB_en_LocaleExtraOverride() throws Exception {
+		Resources res = new Resources("resources/test6.json");
+		assertResources(res, "XSGML",
+				"eXtra Standard Generalized Markup Language");
+
+	}
+
+	public void testSerialisationException() throws Exception {
+
+		try {
+			new Resources("resources/test4.json");
+			fail("Expected SerialisationException");
+		} catch (SerializationException e) {
+		}
+
+	}
+
+	public void testIllegalArgumentException() throws Exception {
+
+		// resource doesn't exist...
+		try {
+			new Resources("resources/test5.json");
+			fail("Expected IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+		}
+
+		// resource exists, but locale is null
+		try {
+			new Resources("resources/test1.json", null);
+			fail("Expected IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
+		}
+
+	}
+
+	private static void assertResources(Resources res, String acronym,
+			String term) {
+		assertTrue(res.containsKey("glossary"));
+
+		Map<String, Object> glossary = (Map<String, Object>) res
+				.get("glossary");
+		assertNotNull(glossary);
+		assertTrue(glossary.containsKey("GlossDiv"));
+
+		Map<String, Object> glossDiv = (Map<String, Object>) glossary
+				.get("GlossDiv");
+		assertNotNull(glossDiv);
+
+		assertEquals("S", glossDiv.get("title"));
+
+		assertTrue(glossDiv.containsKey("GlossList"));
+		Map<String, Object> glossList = (Map<String, Object>) glossDiv
+				.get("GlossList");
+		assertNotNull(glossList);
+
+		assertTrue(glossList.containsKey("GlossEntry"));
+		Map<String, Object> glossEntry = (Map<String, Object>) glossList
+				.get("GlossEntry");
+		assertNotNull(glossEntry);
+
+		assertEquals(acronym, glossEntry.get("Acronym"));
+		assertEquals(term, glossEntry.get("GlossTerm"));
+
+		assertTrue(glossEntry.containsKey("GlossDef"));
+		Map<String, Object> glossDef = (Map<String, Object>) glossEntry
+				.get("GlossDef");
+		assertNotNull(glossDef);
+
+		assertTrue(glossDef.containsKey("GlossSeeAlso"));
+		List<String> list = (List<String>) glossDef.get("GlossSeeAlso");
+		assertNotNull(list);
+		assertEquals(2, list.getLength());
+	}
+
+}
--- core/src/pivot/util/Resources.java
+++ core/src/pivot/util/Resources.java
@@ -1,66 +1,168 @@
 package pivot.util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Locale;
 
 import pivot.collections.Dictionary;
+import pivot.collections.Map;
+import pivot.serialization.JSONSerializer;
+import pivot.serialization.SerializationException;
 
 /**
- * TODO This class will function similarly to java.util.ResourceBundle, only it
- * will load resources from JSON files.
- *
- * It will treat any JSON string that begins with a tilde (~) character as a
- * localized resource. It will prepend an appropriate locale path to the
- * value and convert it to a URL (e.g. /en_GB/foo.png). It will only return
- * valid URLs - if a localized resource with the corresponding name can't be
- * found, it will be converted to null.
+ * This class reads a JSON resource at {@link #baseName} using
+ * {@link ClassLoader#getResourceAsStream(String)}. It applies localisation to
+ * the resource using a method similar to that of
+ * {@link java.util.ResourceBundle} in that it loads the base resource, then
+ * applies a country specified resource over-writing the values in the base
+ * using the country specified. It then does the same for country/language
+ * specific.
+ * 
+ * @see java.util.ResourceBundle
+ * 
+ * @author brindy
  */
 public class Resources implements Dictionary<String, Object> {
-    private String baseName = null;
-    private Locale locale = null;
+	private String baseName = null;
+	private Locale locale = null;
+	private Map<String, Object> resourceMap = null;
 
-    public Resources(String baseName) {
-        this(baseName, Locale.getDefault());
-    }
+	public Resources(String baseName) throws IOException,
+			SerializationException {
+		this(baseName, Locale.getDefault());
+	}
 
-    public Resources(String baseName, Locale locale) {
-        if (locale == null) {
-            throw new IllegalArgumentException("locale is null.");
-        }
+	public Resources(String baseName, Locale locale) throws IOException,
+			SerializationException {
+		if (locale == null) {
+			throw new IllegalArgumentException("locale is null.");
+		}
 
-        this.baseName = baseName;
-        this.locale = locale;
-    }
+		this.baseName = baseName;
+		this.locale = locale;
 
-    public String getBaseName() {
-        return baseName;
-    }
+		resourceMap = readJSONResource(baseName);
 
-    public Locale getLocale() {
-        return locale;
-    }
+		// try to find resource for the country
+		try {
+			String localeName = buildName(baseName, locale.getCountry());
+			applyOverrides(resourceMap, readJSONResource(localeName));
+		} catch (IllegalArgumentException e) {
+			// this could happen, a warning will have already been generated
+		}
 
-    public Object get(String key) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+		// try to find resource for language in the country
+		try {
+			String localeName = buildName(baseName, locale.getCountry(), locale
+					.getLanguage());
+			applyOverrides(resourceMap, readJSONResource(localeName));
+		} catch (IllegalArgumentException e) {
+			// this could happen, a warning will have already been generated
+		}
+	}
 
-    public Object put(String key, Object value) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public boolean containsKey(String key) {
+		return resourceMap.containsKey(key);
+	}
 
-    public Object remove(String key) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public Object get(String key) {
+		return resourceMap.get(key);
+	}
 
-    public boolean containsKey(String key) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	public String getBaseName() {
+		return baseName;
+	}
 
-    public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	public Locale getLocale() {
+		return locale;
+	}
+
+	public boolean isEmpty() {
+		return resourceMap.isEmpty();
+	}
+
+	public Object put(String key, Object value) {
+		throw new UnsupportedOperationException(
+				"Resources instances are immutable");
+	}
+
+	public Object remove(String key) {
+		throw new UnsupportedOperationException(
+				"Resources instances are immutable");
+	}
+
+	private void applyOverrides(Map<String, Object> sourceMap,
+			Map<String, Object> overridesMap) {
+
+		for (String key : sourceMap) {
+			if (overridesMap.containsKey(key)) {
+				// an override is present!
+
+				Object source = sourceMap.get(key);
+				Object override = overridesMap.get(key);
+
+				if (isMap(source, override)) {
+					applyOverrides((Map<String, Object>) source,
+							(Map<String, Object>) override);
+				} else {
+					sourceMap.put(key, overridesMap.get(key));
+				}
+
+			}
+		}
+
+	}
+
+	private String buildName(String baseName, String... extras) {
+
+		// determine the extension, if any
+		String ext = "";
+		String fileName = baseName;
+		int extIndex = baseName.lastIndexOf(".");
+		if (extIndex > -1) {
+			ext = baseName.substring(extIndex);
+			fileName = baseName.substring(0, extIndex);
+		}
+
+		StringBuffer newName = new StringBuffer();
+		newName.append(fileName);
+
+		for (String extra : extras) {
+			newName.append("_").append(extra);
+		}
+		newName.append(ext);
+
+		return newName.toString();
+	}
+
+	private boolean isNullOrMap(Object o) {
+		return null == o || o instanceof Map;
+	}
+
+	private boolean isMap(Object source, Object override) {
+		return isNullOrMap(source) && isNullOrMap(override);
+	}
+
+	private Map<String, Object> readJSONResource(String baseName)
+			throws IOException, SerializationException {
+		InputStream in = getClass().getClassLoader().getResourceAsStream(
+				baseName);
+		if (null == in) {
+			System.out.println("Localisation warning: No resource at "
+					+ baseName);
+			throw new IllegalArgumentException("No resource at " + baseName);
+		}
+		JSONSerializer serializer = new JSONSerializer();
+		Map<String, Object> resourceMap = null;
+		try {
+			resourceMap = (Map<String, Object>) serializer.readObject(in);
+		} finally {
+			try {
+				in.close();
+			} catch (IOException e) {
+				// can't do anything about this at this point, so ignore
+			}
+		}
+		return resourceMap;
+	}
 }
