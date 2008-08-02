b514a576a74e634d6649b7ee32b16e1dd101a17d
Todd Volkert <tvolkert@apache.org>
2008-08-02 20:01:59 +0000

    Added ShadeDecorator
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@753799 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wtk/src/pivot/wtk/effects/ShadeDecorator.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2008 VMware, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pivot.wtk.effects;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import pivot.wtk.Component;
+import pivot.wtk.Decorator;
+
+public class ShadeDecorator implements Decorator {
+    private float opacity;
+    private Color color;
+
+    private transient Graphics2D graphics;
+    private transient Component component;
+
+    /**
+     * Creates a new <tt>ShadeDecorator</tt> with the default opacity and
+     * shade color.
+     */
+    public ShadeDecorator() {
+        this(0.33f, Color.BLACK);
+    }
+
+    /**
+     * Creates a new <tt>ShadeDecorator</tt> with the specified opacity and
+     * shade color.
+     *
+     * @param opacity
+     * The opacity of the shade, between 0 and 1, exclusive.
+     * @param color
+     * The color of the shade.
+     */
+    public ShadeDecorator(float opacity, Color color) {
+        if (opacity <= 0 || opacity >= 1) {
+            throw new IllegalArgumentException
+                ("opacity must be between 0 and 1, exclusive.");
+        }
+
+        if (color == null) {
+            throw new IllegalArgumentException("color is null.");
+        }
+
+        this.opacity = opacity;
+        this.color = color;
+    }
+
+    public Graphics2D prepare(Component component, Graphics2D graphics) {
+        this.graphics = graphics;
+        this.component = component;
+
+        return graphics;
+    }
+
+    public void update() {
+        graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
+            opacity));
+        graphics.setColor(color);
+        graphics.fillRect(0, 0, component.getWidth(), component.getHeight());
+    }
+}
