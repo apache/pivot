f17e7d18d868562f49ea7025819ca118a66b2710
eryzhikov <dev-null@apache.org>
2008-09-10 04:17:55 +0000

    Added UI component highliting
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@753973 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/src/pivot/tools/explorer/ComponentHighlighter.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package pivot.tools.explorer;
+
+import java.awt.Color;
+
+import pivot.collections.Sequence;
+import pivot.tools.explorer.tree.TreeNodeList;
+import pivot.wtk.Component;
+import pivot.wtk.ComponentMouseListener;
+import pivot.wtk.Decorator;
+import pivot.wtk.TreeView;
+import pivot.wtk.effects.ShadeDecorator;
+
+//TODO: Color and opacity should come from preferences
+final class ComponentHighlighter implements ComponentMouseListener {
+
+	private final TreeView tree;
+	private Component lastDecoratedComponent;
+	private Decorator componentDecorator = new ShadeDecorator(0.33f, Color.CYAN.brighter());
+
+	public ComponentHighlighter(TreeView tree) {
+		this.tree = tree;
+	}
+
+	public void mouseMove(Component component, int x, int y) {
+
+		if (component == tree) {
+			Sequence<Integer> nodeAt = tree.getNodeAt(y);
+			if (nodeAt.getLength() != 0) {
+				Sequence<ComponentAdapter> nodePath = TreeNodeList.create(tree, nodeAt);
+				ComponentAdapter node = nodePath.get(nodePath.getLength() - 1);
+
+				highlightComponent(node.getComponent());
+				return;
+			}
+		}
+		highlightComponent(null);
+
+	}
+
+	public void mouseOut(Component component) {
+		highlightComponent(null);
+	}
+
+	public void mouseOver(Component component) {
+	}
+
+	private void highlightComponent(Component component) {
+
+		if (lastDecoratedComponent != null) {
+			lastDecoratedComponent.getDecorators().remove(componentDecorator);
+		}
+
+		if (component != null) {
+			component.getDecorators().add(componentDecorator);
+		}
+
+		lastDecoratedComponent = component;
+
+	}
+}
--- tools/src/pivot/tools/explorer/Explorer.java
+++ tools/src/pivot/tools/explorer/Explorer.java
@@ -39,7 +39,7 @@ public class Explorer implements Application, TreeViewSelectionListener {
     private TreeView componentTree;
     private TableView propertiesTable, stylesTable, attributesTable;
     private Label statusLabel;
-    private Component attributesTab;
+    private Component attributesTab; 
 
     public void startup(Display display, Dictionary<String, String> properties) throws Exception {
 
@@ -148,9 +148,14 @@ public class Explorer implements Application, TreeViewSelectionListener {
         return dialog;
 
     }
+    
 
-    private void initComponentTree(TreeView tree, Iterable<Component> components) {
+    private void initComponentTree(final TreeView tree, Iterable<Component> components) {
+
     	tree.getTreeViewSelectionListeners().add(this);
+    	
+    	//TODO: use preferences to toggle highlighting
+    	tree.getComponentMouseListeners().add(new ComponentHighlighter(tree));
 
         // build tree data
         List<ComponentAdapter> componentList = new ArrayList<ComponentAdapter>();
@@ -193,6 +198,6 @@ public class Explorer implements Application, TreeViewSelectionListener {
         stylesTable.getColumns().get(1).setCellRenderer( cellRenderer );
         attributesTable.getColumns().get(1).setCellRenderer( cellRenderer );
 
-    }
+    }    
 
 }
