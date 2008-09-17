86c2938bde6e873f1cb57aa542718e71614c2fcd
gkbrown <dev-null@apache.org>
2008-09-17 12:42:41 +0000

    Update menu API to prepare for menu implementation.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@754009 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wtk/src/pivot/wtk/skin/terra/MenuBarItemSkin.java
@@ -0,0 +1,50 @@
+package pivot.wtk.skin.terra;
+
+import java.awt.Graphics2D;
+
+import pivot.wtk.Component;
+import pivot.wtk.Dimensions;
+import pivot.wtk.Menu;
+import pivot.wtk.skin.ButtonSkin;
+
+public class MenuBarItemSkin extends ButtonSkin {
+    @Override
+    public void install(Component component) {
+        validateComponentType(component, Menu.Item.class);
+
+        super.install(component);
+
+        // TODO
+    }
+
+    @Override
+    public void uninstall() {
+        // TODO
+
+        super.uninstall();
+    }
+
+    public int getPreferredWidth(int height) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public int getPreferredHeight(int width) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public Dimensions getPreferredSize() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void layout() {
+        // No-op
+    }
+
+    public void paint(Graphics2D graphics) {
+        // TODO Auto-generated method stub
+
+    }
+}
