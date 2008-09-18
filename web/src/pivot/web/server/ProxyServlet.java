e7afffdeb05f16b7c52ba5a92ff500f5e5d33b25
Todd Volkert <tvolkert@apache.org>
2008-09-18 17:43:13 +0000

    Moved ProxyServlet to pivot.web.server
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@754020 13f79535-47bb-0310-9956-ffa450edef68

--- web/src/pivot/web/server/ProxyServlet.java
+++ web/src/pivot/web/server/ProxyServlet.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package pivot.web.proxy;
+package pivot.web.server;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
