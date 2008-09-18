0d66f6fccd680d1bab7b2d370055d56c65637992
Todd Volkert <tvolkert@apache.org>
2008-09-18 21:58:11 +0000

    Added QueryServlet, and migrated pivot.wev.proxy to pivot.web.server
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@754022 13f79535-47bb-0310-9956-ffa450edef68

---
+++ web/src/pivot/web/server/QueryServlet.java
@@ -0,0 +1,538 @@
+/*
+ * Copyright (c) 2008 VMware, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pivot.web.server;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.Enumeration;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pivot.collections.Dictionary;
+import pivot.collections.HashMap;
+import pivot.serialization.JSONSerializer;
+import pivot.serialization.SerializationException;
+import pivot.serialization.Serializer;
+import pivot.util.Base64;
+
+/**
+ * Base class for all web query servlets. It is the server counterpart to
+ * {@link pivot.web.Query}. Subclasses will generally only have to override one
+ * or more of the following methods:
+ * <ul>
+ *   <li>{@link #doGet()}</li>
+ *   <li>{@link #doPost(Object)}</li>
+ *   <li>{@link #doPut(Object)}</li>
+ *   <li>{@link #doDelete()}</li>
+ * </ul>
+ *
+ * @author tvolkert
+ */
+public abstract class QueryServlet extends HttpServlet {
+    static final long serialVersionUID = -646654620936816287L;
+
+    /**
+     * User credentials, which will be made availale if the servlet's
+     * <tt>authenticationRequired</tt> flag is set to <tt>true</tt>.
+     *
+     * @author tvolkert
+     */
+    public static final class Credentials {
+        private String username;
+        private String password;
+
+        private Credentials(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+
+    /**
+     * Arguments dictionary implementation.
+     *
+     * @author tvolkert
+     */
+    public final class ArgumentsDictionary
+        implements Dictionary<String, String>, Iterable<String> {
+        public String get(String key) {
+            return arguments.get(key);
+        }
+
+        public String put(String key, String value) {
+            throw new UnsupportedOperationException();
+        }
+
+        public String remove(String key) {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean containsKey(String key) {
+            return arguments.containsKey(key);
+        }
+
+        public boolean isEmpty() {
+            return arguments.isEmpty();
+        }
+
+        public Iterator<String> iterator() {
+            return arguments.iterator();
+        }
+    }
+
+    /**
+     * Request properties dictionary implementation.
+     *
+     * @author tvolkert
+     */
+    public final class RequestPropertiesDictionary
+        implements Dictionary<String, String>, Iterable<String> {
+        public String get(String key) {
+            return requestProperties.get(key);
+        }
+
+        public String put(String key, String value) {
+            throw new UnsupportedOperationException();
+        }
+
+        public String remove(String key) {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean containsKey(String key) {
+            return requestProperties.containsKey(key);
+        }
+
+        public boolean isEmpty() {
+            return requestProperties.isEmpty();
+        }
+
+        public Iterator<String> iterator() {
+            return requestProperties.iterator();
+        }
+    }
+
+    /**
+     * Response properties dictionary implementation.
+     *
+     * @author tvolkert
+     */
+    public final class ResponsePropertiesDictionary
+        implements Dictionary<String, String>, Iterable<String> {
+        public String get(String key) {
+            return responseProperties.get(key);
+        }
+
+        public String put(String key, String value) {
+            return responseProperties.put(key, value);
+        }
+
+        public String remove(String key) {
+            return responseProperties.remove(key);
+        }
+
+        public boolean containsKey(String key) {
+            return responseProperties.containsKey(key);
+        }
+
+        public boolean isEmpty() {
+            return responseProperties.isEmpty();
+        }
+
+        public Iterator<String> iterator() {
+            return responseProperties.iterator();
+        }
+    }
+
+    private boolean authenticationRequired = false;
+    private Credentials credentials = null;
+
+    private String hostname;
+    private String contextPath;
+    private String queryPath;
+    private int port;
+    private boolean secure;
+
+    private HashMap<String, String> arguments = new HashMap<String, String>();
+    private HashMap<String, String> requestProperties = new HashMap<String, String>();
+    private HashMap<String, String> responseProperties = new HashMap<String, String>();
+
+    private ArgumentsDictionary argumentsDictionary = new ArgumentsDictionary();
+    private RequestPropertiesDictionary requestPropertiesDictionary = new RequestPropertiesDictionary();
+    private ResponsePropertiesDictionary responsePropertiesDictionary = new ResponsePropertiesDictionary();
+
+    private Serializer serializer = new JSONSerializer();
+
+    private static final String BASIC_AUTHENTICATION_TAG = "Basic";
+    private static final String HTTPS_PROTOCOL = "https";
+    private static final String URL_ENCODING = "UTF-8";
+
+    /**
+     * Gets the host name that was requested.
+     */
+    public String getHostname() {
+        return hostname;
+    }
+
+    /**
+     * Returns the portion of the request URI that indicates the context of the
+     * request. The context path always comes first in a request URI. The path
+     * starts with a "/" character but does not end with a "/" character. For
+     * servlets in the default (root) context, this method returns "".
+     */
+    public String getContextPath() {
+        return contextPath;
+    }
+
+    /**
+     * Returns the portion of the request URI that occurs after the context
+     * path but preceding the query string. It will start with a "/" character.
+     * For servlets in the default (root) context, this method returns the full
+     * path.
+     */
+    public String getQueryPath() {
+        return queryPath;
+    }
+
+    /**
+     * Returns the Internet Protocol (IP) port number of the interface on which
+     * the request was received.
+     */
+    public int getPort() {
+        return port;
+    }
+
+    /**
+     * Tells whether the request has been ecrypted over HTTPS.
+     */
+    public boolean isSecure() {
+        return secure;
+    }
+
+    /**
+     * Tells whether or not this servlet will require authentication data. If
+     * set to <tt>true</tt>, and un-authenticated requests are received, the
+     * servlet will automatically respond with a request for authentication.
+     */
+    public boolean isAuthenticationRequired() {
+        return authenticationRequired;
+    }
+
+    /**
+     * Sets whether or not this servlet will require authentication data. If
+     * set to <tt>true</tt>, and un-authenticated requests are received, the
+     * servlet will automatically respond with a request for authentication.
+     */
+    public void setAuthenticationRequired(boolean authenticationRequired) {
+        this.authenticationRequired = authenticationRequired;
+
+        if (!authenticationRequired) {
+            credentials = null;
+        }
+    }
+
+    /**
+     * Gets the authentication credentials that were extracted from the
+     * request. These are only available if the <tt>authenticationRequired</tt>
+     * flag is set to <tt>true</tt>.
+     */
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    /**
+     * Returns the servlet's arguments dictionary, which holds the values
+     * passed in the HTTP request query string.
+     */
+    public ArgumentsDictionary getArguments() {
+        return argumentsDictionary;
+    }
+
+    /**
+     * Returns the servlet's request property dictionary, which holds the HTTP
+     * request headers.
+     */
+    public RequestPropertiesDictionary getRequestProperties() {
+        return requestPropertiesDictionary;
+    }
+
+    /**
+     * Returns the servlet's response property dictionary, which holds the HTTP
+     * response headers that will be sent back to the client.
+     */
+    public ResponsePropertiesDictionary getResponseProperties() {
+        return responsePropertiesDictionary;
+    }
+
+    /**
+     * Returns the serializer used to stream the value passed to or from the
+     * web query. By default, an instance of {@link JSONSerializer} is used.
+     */
+    public Serializer getSerializer() {
+        return serializer;
+    }
+
+    /**
+     * Sets the serializer used to stream the value passed to or from the
+     * web query.
+     *
+     * @param serializer
+     */
+    public void setSerializer(Serializer serializer) {
+        if (serializer == null) {
+            throw new IllegalArgumentException("serializer is null.");
+        }
+
+        this.serializer = serializer;
+    }
+
+    /**
+     *
+     */
+    protected Object doGet() throws ServletException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     */
+    protected URL doPost(Object value) throws ServletException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     */
+    protected void doPut(Object value) throws ServletException {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     */
+    protected void doDelete() throws ServletException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        boolean proceed = true;
+
+        if (authenticationRequired) {
+            String authorization = request.getHeader("Authorization");
+
+            if (authorization == null) {
+                proceed = false;
+
+                response.setHeader("WWW-Authenticate", "BASIC realm=\""
+                    + request.getServletPath() +"\"");
+                response.setStatus(401);
+                response.setContentLength(0);
+                response.flushBuffer();
+            } else {
+                String encodedCredentials = authorization.substring
+                    (BASIC_AUTHENTICATION_TAG.length() + 1);
+                String decodedCredentials = new String(Base64.decode(encodedCredentials));
+                String[] credentialsPair = decodedCredentials.split(":");
+
+                String username = credentialsPair[0];
+                String password;
+
+                if (credentialsPair.length > 1) {
+                    password = credentialsPair[1];
+                } else {
+                    password = "";
+                }
+
+                if (credentials == null) {
+                    credentials = new Credentials(username, password);
+                } else {
+                    credentials.username = username;
+                    credentials.password = password;
+                }
+            }
+        }
+
+        if (proceed) {
+            // Extract our location context
+            try {
+                URL url = new URL(request.getRequestURL().toString());
+
+                hostname = url.getHost();
+                contextPath = request.getContextPath();
+                queryPath = request.getRequestURI();
+                port = request.getLocalPort();
+                secure = url.getProtocol().equalsIgnoreCase(HTTPS_PROTOCOL);
+
+                if (queryPath.startsWith(contextPath)) {
+                    queryPath = queryPath.substring(contextPath.length());
+                }
+            } catch (MalformedURLException ex) {
+                throw new ServletException(ex);
+            }
+
+            // Clear out any remnants of the previous service
+            arguments.clear();
+            requestProperties.clear();
+            responseProperties.clear();
+
+            // Copy the query string into our arguments dictionary
+            String queryString = request.getQueryString();
+            if (queryString != null) {
+                String[] pairs = queryString.split("&");
+
+                for (int i = 0, n = pairs.length; i < n; i++) {
+                    String[] pair = pairs[i].split("=");
+
+                    String key = URLDecoder.decode(pair[0], URL_ENCODING);
+                    String value = URLDecoder.decode((pair.length > 1) ? pair[1] : "", URL_ENCODING);
+
+                    arguments.put(key, value);
+                }
+            }
+
+            // Copy the request headers into our request properties dictionary
+            Enumeration<String> headerNames = request.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                String headerValue = request.getHeader(headerName);
+
+                requestProperties.put(headerName, headerValue);
+            }
+
+            super.service(request, response);
+        }
+    }
+
+    @Override
+    protected final void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        try {
+            Object result = doGet();
+
+            response.setStatus(200);
+            setResponseHeaders(response);
+            response.setContentType(serializer.getMIMEType());
+            serializer.writeObject(result, response.getOutputStream());
+        } catch (UnsupportedOperationException ex) {
+            doMethodNotAllowed(response);
+        } catch (SerializationException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    @Override
+    protected final void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        try {
+            Object value = serializer.readObject(request.getInputStream());
+
+            URL url = doPost(value);
+
+            response.setStatus(201);
+            response.setHeader("Location", url.toString());
+            response.setContentLength(0);
+            response.flushBuffer();
+        } catch (UnsupportedOperationException ex) {
+            doMethodNotAllowed(response);
+        } catch (SerializationException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    @Override
+    protected final void doPut(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        try {
+            Object value = serializer.readObject(request.getInputStream());
+
+            doPut(value);
+
+            response.setStatus(204);
+            response.setContentLength(0);
+            response.flushBuffer();
+        } catch (UnsupportedOperationException ex) {
+            doMethodNotAllowed(response);
+        } catch (SerializationException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    @Override
+    protected final void doDelete(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        try {
+            doDelete();
+
+            response.setStatus(204);
+            response.setContentLength(0);
+            response.flushBuffer();
+        } catch (UnsupportedOperationException ex) {
+            doMethodNotAllowed(response);
+        }
+    }
+
+    @Override
+    protected final void doHead(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        doMethodNotAllowed(response);
+    }
+
+    @Override
+    protected final void doOptions(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        doMethodNotAllowed(response);
+    }
+
+    @Override
+    protected final void doTrace(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        doMethodNotAllowed(response);
+    }
+
+    /**
+     *
+     */
+    private void doMethodNotAllowed(HttpServletResponse response) throws IOException {
+        response.setStatus(405);
+        response.setContentLength(0);
+        response.flushBuffer();
+    }
+
+    /**
+     *
+     */
+    private void setResponseHeaders(HttpServletResponse response) {
+        for (String key : responseProperties) {
+            response.setHeader(key, responseProperties.get(key));
+        }
+    }
+}
