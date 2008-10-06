2aa53fe62b8746e609fdd301126c7c3ec73b81d7
Todd Volkert <tvolkert@apache.org>
2008-10-06 21:27:46 +0000

    Added swing adapter test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@754139 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wtk-test/src/pivot/wtk/test/SwingAdapterTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2008 VMware, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pivot.wtk.test;
+
+import pivot.collections.Dictionary;
+import pivot.wtk.Application;
+import pivot.wtk.Display;
+import pivot.wtk.Frame;
+import pivot.wtkx.WTKXSerializer;
+
+public class SwingAdapterTest implements Application {
+    private Frame frame = null;
+
+    public void startup(Display display, Dictionary<String, String> properties)
+        throws Exception {
+        WTKXSerializer wtkxSerializer = new WTKXSerializer();
+        frame = (Frame)wtkxSerializer.readObject(getClass().getResource("swing_adapter_test.wtkx"));
+        frame.open(display);
+    }
+
+    public boolean shutdown(boolean optional) {
+        frame.close();
+        return true;
+    }
+
+    public void resume() {
+    }
+
+
+    public void suspend() {
+    }
+}
