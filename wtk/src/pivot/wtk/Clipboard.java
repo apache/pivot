4fdbb017a3b62648cb188327e4cabd4bdf317f4e
gkbrown@mac.com <dev-null@apache.org>
2008-11-24 17:17:48 +0000

    Provide trusted applications with access to the system clipboard.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@754319 13f79535-47bb-0310-9956-ffa450edef68

--- wtk/src/pivot/wtk/Clipboard.java
+++ wtk/src/pivot/wtk/Clipboard.java
@@ -15,163 +15,64 @@
  */
 package pivot.wtk;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
-
-import pivot.collections.Dictionary;
-import pivot.collections.HashMap;
-import pivot.serialization.SerializationException;
-import pivot.serialization.Serializer;
-import pivot.serialization.JSONSerializer;
 
 /**
  * Singleton class providing a means of sharing data between components and
  * applications.
- * <p>
- * TODO Copy data to system clipboard when allowed (and the data format is
- * supported).
- * <p>
- * TODO Use a list of Class:Serializer mapping, so we can prioritize? Using
- * this approach, we could walk the list and use the first matching serializer
- * (e.g. to use BinarySerializer for classes that implement Serializable).
- * Alternatively, this would let us serialize to all matching types (though
- * this could be a peformance issue).
- * <p>
- * Another alternative is to require the caller to specify the serializer(s)
- * at put() time.
+ *
+ * @author gbrown
  */
 public class Clipboard {
-    /**
-     * Serializer dictionary implementation.
-     *
-     * @author gbrown
-     */
-    public static final class SerializerDictionary
-        implements Dictionary<Class<?>, Class<? extends Serializer>> {
-        public Class<? extends Serializer> get(Class<?> type) {
-            // TODO Walk class hierarchy to find a match
-
-            return serializers.get(type);
-        }
-
-        public Class<? extends Serializer> put(Class<?> type,
-            Class<? extends Serializer> serializerType) {
-            if (serializerType == null) {
-                throw new IllegalArgumentException("serializerType is null.");
-            }
-
-            return serializers.put(type, serializerType);
-        }
-
-        public Class<? extends Serializer> remove(Class<?> type) {
-            return serializers.remove(type);
-        }
-
-        public boolean containsKey(Class<?> type) {
-            return serializers.containsKey(type);
-        }
-
-        public boolean isEmpty() {
-            return serializers.isEmpty();
-        }
-    }
-
-    private Class<?> type = null;
-    private byte[] buffer = null;
-
+    private static java.awt.datatransfer.Clipboard awtClipboard;
     private static Clipboard instance = new Clipboard();
 
-    private static HashMap<Class<?>, Class<? extends Serializer>> serializers = new HashMap<Class<?>, Class<? extends Serializer>>();
-    private static SerializerDictionary serializerDictionary = new SerializerDictionary();
-
-    public void put(Object contents) {
-        if (contents == null) {
-            throw new IllegalArgumentException("contents is null");
-        }
-
-        Class<?> type = contents.getClass();
-
-        if (!serializers.containsKey(type)) {
-            throw new IllegalArgumentException("No serializer found for "
-                + type.getName());
-        }
-
-        Class<? extends Serializer> serializerType = serializers.get(type);
-
-        Serializer serializer = null;
+    public Clipboard() {
         try {
-            serializer = serializerType.newInstance();
-        } catch(IllegalAccessException exception) {
-            throw new RuntimeException(exception);
-        } catch(InstantiationException exception) {
-            throw new RuntimeException(exception);
+            awtClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        } catch(SecurityException exception) {
+            System.out.println("Access to system clipboard is not allowed; using local clipboard.");
+            awtClipboard = new java.awt.datatransfer.Clipboard(null);
         }
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-
-        try {
-            serializer.writeObject(contents, byteArrayOutputStream);
-        } catch(IOException exception) {
-            throw new RuntimeException(exception);
-        } catch(SerializationException exception) {
-            throw new RuntimeException(exception);
-        }
-
-        this.type = type;
-        buffer = byteArrayOutputStream.toByteArray();
     }
 
     public Object get() {
         Object contents = null;
 
-        if (buffer != null) {
-            if (!serializers.containsKey(type)) {
-                throw new IllegalStateException("No serializer found for "
-                    + type.getName());
-            }
-
-            Class<? extends Serializer> serializerType = serializers.get(type);
-
-            Serializer serializer = null;
-            try {
-                serializer = serializerType.newInstance();
-            } catch(IllegalAccessException exception) {
-                throw new RuntimeException(exception);
-            } catch(InstantiationException exception) {
-                throw new RuntimeException(exception);
-            }
-
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(buffer);
+        if (awtClipboard.isDataFlavorAvailable(DataFlavor.stringFlavor)) {
+            Transferable systemClipboardContents = awtClipboard.getContents(this);
 
             try {
-                contents = serializer.readObject(byteArrayInputStream);
-            } catch(IOException exception) {
-                throw new RuntimeException(exception);
-            } catch(SerializationException exception) {
-                throw new RuntimeException(exception);
+                contents = systemClipboardContents.getTransferData(DataFlavor.stringFlavor);
+            } catch (UnsupportedFlavorException exception) {
+                System.out.println(exception);
+            } catch (IOException exception) {
+                System.out.println(exception);
             }
         }
 
         return contents;
     }
 
-    public void clear() {
-        type = null;
-        buffer = null;
+    public void put(Object contents) {
+        if (contents == null) {
+            throw new IllegalArgumentException("contents is null");
+        }
+
+        if (!(contents instanceof String)) {
+            throw new IllegalArgumentException("Only string content is currently supported.");
+        }
+
+        StringSelection stringSelection = new StringSelection((String)contents);
+        awtClipboard.setContents(stringSelection, stringSelection);
     }
 
     public static Clipboard getInstance() {
         return instance;
-    }
-
-    public static SerializerDictionary getSerializers() {
-        return serializerDictionary;
-    }
-
-    static {
-        serializers.put(String.class, JSONSerializer.class);
-        serializers.put(Number.class, JSONSerializer.class);
-        serializers.put(Boolean.class, JSONSerializer.class);
     }
 }
