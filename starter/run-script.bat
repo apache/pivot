a2c89afa428dc10c5eb9c63b919951855acbc18b
gkbrown@mac.com <dev-null@apache.org>
2009-03-10 21:02:55 +0000

    Remove starter project; this project will be recreated for version 1.1.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@754437 13f79535-47bb-0310-9956-ffa450edef68

--- starter/run-script.bat
+++
@@ -1,1 +0,0 @@
-@java -cp lib/pivot-core.jar;lib/pivot-web.jar;lib/pivot-wtk.jar;lib/pivot-wtk.terra.jar;lib/stax-1.2.0.jar;lib/stax-api-1.0.jar;lib/js-engine.jar;lib/js.jar;script pivot.wtk.DesktopApplicationContext pivot.wtk.ScriptApplication src:starter.wtkx title:"Pivot Starter Application - JavaScript"
