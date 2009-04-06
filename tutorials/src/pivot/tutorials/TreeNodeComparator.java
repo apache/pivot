4de7dc1fe6b563c8ee567ef7c24b47ddb6b2be0e
Todd Volkert <tvolkert@apache.org>
2009-04-06 16:40:20 +0000

    Added sorting to the editable tree in the kitchen sink demo
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@762412 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tutorials/src/pivot/tutorials/TreeNodeComparator.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pivot.tutorials;
+
+import java.util.Comparator;
+
+import pivot.wtk.content.TreeNode;
+
+/**
+ * Orders TreeNode instances by their name using string comparison.
+ *
+ * @author tvolkert
+ */
+public class TreeNodeComparator implements Comparator<TreeNode> {
+    public int compare(TreeNode treeNode1, TreeNode treeNode2) {
+        String text1 = treeNode1.getText();
+        String text2 = treeNode2.getText();
+
+        int result;
+
+        if (text1 == null && text2 == null) {
+            result = 0;
+        } else if (text1 == null) {
+            result = -1;
+        } else if (text2 == null) {
+            result = 1;
+        } else {
+            result = text1.compareTo(text2);
+        }
+
+        return result;
+    }
+}
