54093ad9899c5c2ad34ed8e134e6c1b18d8db3cb
Gregory K. Brown <gbrown@apache.org>
2009-04-07 16:48:11 +0000

    Update FileBrowserTest to work correctly with recent Folder updates.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@762843 13f79535-47bb-0310-9956-ffa450edef68

--- wtk-test/src/pivot/wtk/test/FileBrowserTest.java
+++ wtk-test/src/pivot/wtk/test/FileBrowserTest.java
@@ -17,53 +17,48 @@
 package pivot.wtk.test;
 
 import pivot.collections.Dictionary;
+import pivot.collections.Sequence;
 import pivot.io.Folder;
 import pivot.wtk.Application;
 import pivot.wtk.Component;
 import pivot.wtk.Display;
-import pivot.wtk.FlowPane;
 import pivot.wtk.Frame;
-import pivot.wtk.TextInput;
 import pivot.wtk.TreeView;
+import pivot.wtk.TreeViewBranchListener;
 import pivot.wtkx.WTKXSerializer;
 
 public class FileBrowserTest implements Application {
-    private Frame frame1 = null;
-    private Frame frame2 = null;
-    private Frame frame3 = null;
+    private Frame frame = null;
 
     public void startup(Display display, Dictionary<String, String> properties)
         throws Exception {
         WTKXSerializer wtkxSerializer = new WTKXSerializer();
-        frame1 = new Frame((Component)wtkxSerializer.readObject(getClass().getResource("file_browser_test.wtkx")));
+        frame = new Frame((Component)wtkxSerializer.readObject(getClass().getResource("file_browser_test.wtkx")));
 
         TreeView folderTreeView = (TreeView)wtkxSerializer.getObjectByName("folderTreeView");
 
         String pathname = "/";
-        folderTreeView.setTreeData(new Folder(pathname));
+        final Folder rootFolder = new Folder(pathname);
+        rootFolder.refresh();
+        folderTreeView.setTreeData(rootFolder);
+        folderTreeView.getTreeViewBranchListeners().add(new TreeViewBranchListener() {
+            public void branchExpanded(TreeView treeView, Sequence<Integer> path) {
+                Folder folder = (Folder)Sequence.Tree.get(rootFolder, path);
+                folder.refresh();
+            }
 
-        frame1.setTitle("File Browser Test");
-        frame1.setPreferredSize(480, 640);
-        frame1.open(display);
+            public void branchCollapsed(TreeView treeView, Sequence<Integer> path) {
+                // No-op
+            }
+        });
 
-        FlowPane flowPane2 = new FlowPane();
-        flowPane2.add(new TextInput());
-        frame2 = new Frame(flowPane2);
-        frame2.setTitle("Frame 2");
-        frame2.setPreferredSize(240, 80);
-        frame2.open(display);
-
-        FlowPane flowPane3 = new FlowPane();
-        flowPane3.add(new TextInput());
-        frame3 = new Frame(flowPane3);
-        frame3.setTitle("Frame 3");
-        frame3.setPreferredSize(240, 80);
-        frame3.open(display);
+        frame.setTitle("File Browser Test");
+        frame.setPreferredSize(480, 640);
+        frame.open(display);
     }
 
     public boolean shutdown(boolean optional) {
-        frame1.close();
-        frame2.close();
+        frame.close();
         return true;
     }
 
