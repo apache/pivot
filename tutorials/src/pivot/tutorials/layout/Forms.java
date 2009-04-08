d10a7087031976c2601d18a4467ff65e0534b7a3
Gregory K. Brown <gbrown@apache.org>
2009-04-08 20:44:00 +0000

    Add forms tutorial example.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@763382 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tutorials/src/pivot/tutorials/layout/Forms.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pivot.tutorials.layout;
+
+import pivot.collections.Dictionary;
+import pivot.wtk.Application;
+import pivot.wtk.Button;
+import pivot.wtk.ButtonPressListener;
+import pivot.wtk.Component;
+import pivot.wtk.Display;
+import pivot.wtk.FlowPane;
+import pivot.wtk.Form;
+import pivot.wtk.Label;
+import pivot.wtk.MessageType;
+import pivot.wtk.Prompt;
+import pivot.wtk.PushButton;
+import pivot.wtk.TextInput;
+import pivot.wtk.Window;
+import pivot.wtkx.WTKXSerializer;
+
+public class Forms implements Application {
+    private Window window = null;
+    private FlowPane nameFlowPane = null;
+    private TextInput lastNameTextInput = null;
+    private TextInput firstNameTextInput = null;
+    private Label errorLabel = null;
+
+    public void startup(Display display, Dictionary<String, String> properties) throws Exception {
+        WTKXSerializer wtkxSerializer = new WTKXSerializer();
+        window = new Window((Component)wtkxSerializer.readObject(getClass().getResource("forms.wtkx")));
+
+        nameFlowPane = (FlowPane)wtkxSerializer.getObjectByName("nameFlowPane");
+        lastNameTextInput = (TextInput)wtkxSerializer.getObjectByName("lastNameTextInput");
+        firstNameTextInput = (TextInput)wtkxSerializer.getObjectByName("firstNameTextInput");
+
+        PushButton submitButton = (PushButton)wtkxSerializer.getObjectByName("submitButton");
+        submitButton.getButtonPressListeners().add(new ButtonPressListener() {
+            public void buttonPressed(Button button) {
+                String lastName = lastNameTextInput.getText();
+                String firstName = firstNameTextInput.getText();
+
+                Form.Flag flag = null;
+                if (lastName.length() == 0
+                    || firstName.length() == 0) {
+                    flag = new Form.Flag(MessageType.ERROR, "Name is required.");
+                }
+
+                Form.setFlag(nameFlowPane, flag);
+
+                if (flag == null) {
+                    errorLabel.setText(null);
+                    Prompt.prompt("Pretending to submit...", window);
+                } else {
+                    errorLabel.setText("Some required information is missing.");
+                }
+            }
+        });
+
+        errorLabel = (Label)wtkxSerializer.getObjectByName("errorLabel");
+
+        window.setMaximized(true);
+        window.open(display);
+    }
+
+    public boolean shutdown(boolean optional) {
+        window.close();
+        return true;
+    }
+
+    public void suspend() {
+    }
+
+    public void resume() {
+    }
+}
