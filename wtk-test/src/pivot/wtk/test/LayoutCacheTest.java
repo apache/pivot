25836b0c663320c9f01270c5f6beef0da80c1943
Todd Volkert <tvolkert@apache.org>
2009-04-09 19:22:56 +0000

    Added gradient to layout cache test, to allow it to be used to test the viewport optimization
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@763775 13f79535-47bb-0310-9956-ffa450edef68

--- wtk-test/src/pivot/wtk/test/LayoutCacheTest.java
+++ wtk-test/src/pivot/wtk/test/LayoutCacheTest.java
@@ -16,9 +16,13 @@
  */
 package pivot.wtk.test;
 
+import java.awt.Color;
+import java.awt.GradientPaint;
+
 import pivot.collections.Dictionary;
 import pivot.wtk.Application;
 import pivot.wtk.Display;
+import pivot.wtk.ScrollPane;
 import pivot.wtk.Window;
 import pivot.wtkx.WTKXSerializer;
 
@@ -33,6 +37,11 @@ public class LayoutCacheTest implements Application {
     public void startup(Display display, Dictionary<String, String> properties) throws Exception {
         WTKXSerializer wtkxSerializer = new WTKXSerializer();
         window = (Window)wtkxSerializer.readObject(getClass().getResource("layoutCacheTest.wtkx"));
+
+        ScrollPane scrollPane = (ScrollPane)wtkxSerializer.getObjectByName("scrollPane");
+        scrollPane.getStyles().put("backgroundPaint", new GradientPaint(0, 0,
+            new Color(0x7e, 0xb4, 0xda), 0, 1000, new Color(0x00, 0x25, 0x3f)));
+
         window.open(display);
     }
 
