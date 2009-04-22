01ebe9327da7a8c34d76b741de26805e03773043
Gregory K. Brown <gbrown@apache.org>
2009-04-22 13:31:06 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@767513 13f79535-47bb-0310-9956-ffa450edef68

--- wtk/src/pivot/wtk/skin/ScrollPaneSkin.java
+++ wtk/src/pivot/wtk/skin/ScrollPaneSkin.java
@@ -19,7 +19,6 @@ package pivot.wtk.skin;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Paint;
-import java.awt.Transparency;
 
 import pivot.wtk.Component;
 import pivot.wtk.Dimensions;
@@ -59,7 +58,7 @@ public class ScrollPaneSkin extends ContainerSkin
     private int cachedHorizontalScrollBarHeight = 0;
     private int cachedVerticalScrollBarWidth = 0;
 
-    private boolean optimizeScrolling = true;
+    private boolean optimizeScrolling = false;
 
     private static final int DEFAULT_HORIZONTAL_INCREMENT = 10;
     private static final int DEFAULT_VERTICAL_INCREMENT = 10;
@@ -808,8 +807,11 @@ public class ScrollPaneSkin extends ContainerSkin
     public void setBackgroundPaint(Paint backgroundPaint) {
         super.setBackgroundPaint(backgroundPaint);
 
+        // TODO
+        /*
         optimizeScrolling = (backgroundPaint instanceof Color &&
             backgroundPaint.getTransparency() == Transparency.OPAQUE);
+        */
     }
 
     public int getHorizontalIncrement() {
