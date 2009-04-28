42acdcd938cc3de2ec089eab5d16604048a48eb9
Todd Volkert <tvolkert@apache.org>
2009-04-28 02:39:51 +0000

    Changed to use a listener adapter in the TableViewRowEditor to gain the benefit of the @Override annotations
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@769223 13f79535-47bb-0310-9956-ffa450edef68

--- wtk/src/pivot/wtk/content/TableViewRowEditor.java
+++ wtk/src/pivot/wtk/content/TableViewRowEditor.java
@@ -318,23 +318,28 @@ public class TableViewRowEditor implements TableView.RowEditor {
      *
      * @author tvolkert
      */
-    private TableViewRowListener tableViewRowHandler = new TableViewRowListener() {
+    private TableViewRowListener tableViewRowHandler = new TableViewRowListener.Adapter() {
+        @Override
         public void rowInserted(TableView tableView, int index) {
             cancel();
         }
 
+        @Override
         public void rowsRemoved(TableView tableView, int index, int count) {
             cancel();
         }
 
+        @Override
         public void rowsSorted(TableView tableView) {
             cancel();
         }
 
+        @Override
         public void rowsCleared(TableView tableView) {
             cancel();
         }
 
+        @Override
         public void rowUpdated(TableView tableView, int index) {
             cancel();
         }
