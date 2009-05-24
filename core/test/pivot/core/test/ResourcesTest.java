b7eb4a02e7184e50d5ea7700b55d139b60daa609
Christopher Brind <brindy@apache.org>
2009-05-24 16:46:55 +0000

    Fixed test so not dependent on locale in which it is running.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@778193 13f79535-47bb-0310-9956-ffa450edef68

--- core/test/pivot/core/test/ResourcesTest.java
+++ core/test/pivot/core/test/ResourcesTest.java
@@ -16,6 +16,7 @@
  */
 package pivot.core.test;
 
+import java.util.Locale;
 import java.util.MissingResourceException;
 
 import junit.framework.TestCase;
@@ -26,125 +27,147 @@ import pivot.util.Resources;
 
 public class ResourcesTest extends TestCase {
 
-    public void testReadDefaultLocale() throws Exception {
+	private Locale old;
 
-        Resources res = new Resources("resources.test1");
-        assertResources(res, "SGML", "Standard Generalized Markup Language");
-    }
+	@Override
+	protected void setUp() throws Exception {
 
-    /**
-     * The resource overrides the term for the country.
-     *
-     * @throws Exception
-     */
-    public void testRead_GB_Locale() throws Exception {
-        Resources res = new Resources("resources.test2");
-        assertResources(res, "SGML",
-                "How Do, Youth, Standard Generalized Markup Language");
+		old = Locale.getDefault();
+		Locale.setDefault(Locale.UK);
 
-    }
+		super.setUp();
+	}
 
-    /**
-     * The resource overrides the term for the country and the acronym for the
-     * language.
-     *
-     * @throws Exception
-     */
-    public void testRead_GB_en_Locale() throws Exception {
-        Resources res = new Resources("resources.test3");
-        assertResources(res, "XSGML",
-                "How Do, Youth, Standard Generalized Markup Language");
+	@Override
+	protected void tearDown() throws Exception {
+		Locale.setDefault(old);
+		super.tearDown();
+	};
 
-    }
+	public void testReadDefaultLocale() throws Exception {
 
-    /**
-     * The resource overrides the term and the acronym for the country.
-     *
-     * @throws Exception
-     */
-    public void testRead_GB_en_LocaleExtraOverride() throws Exception {
-        Resources res = new Resources("resources.test6");
-        assertResources(res, "XSGML",
-                "eXtra Standard Generalized Markup Language");
+		Resources res = new Resources("resources.test1");
+		assertResources(res, "SGML", "Standard Generalized Markup Language");
+	}
 
-    }
+	/**
+	 * The resource overrides the term for the country.
+	 *
+	 * @throws Exception
+	 */
+	public void testRead_GB_Locale() throws Exception {
+		Resources res = new Resources("resources.test2");
+		assertResources(res, "SGML",
+				"How Do, Youth, Standard Generalized Markup Language");
 
-    public void testSerialisationException() throws Exception {
+	}
 
-        try {
-            new Resources("resources.test4");
-            fail("Expected SerialisationException");
-        } catch (SerializationException e) {
-        }
+	/**
+	 * The resource overrides the term for the country and the acronym for the
+	 * language.
+	 *
+	 * @throws Exception
+	 */
+	public void testRead_GB_en_Locale() throws Exception {
+		assertEquals("Default locale should be en_GB", "en_GB", Locale
+				.getDefault().toString());
+		Resources res = new Resources("resources.test3");
+		assertResources(res, "XSGML",
+				"How Do, Youth, Standard Generalized Markup Language");
 
-    }
+	}
 
-    public void testMissingResource() throws Exception {
+	/**
+	 * The resource overrides the term and the acronym for the country.
+	 *
+	 * @throws Exception
+	 */
+	public void testRead_GB_en_LocaleExtraOverride() throws Exception {
+		assertEquals("Default locale should be en_GB", "en_GB", Locale
+				.getDefault().toString());
+		Resources res = new Resources("resources.test6");
+		assertResources(res, "XSGML",
+				"eXtra Standard Generalized Markup Language");
 
-        // resource doesn't exist...
-        try {
-            new Resources("resources.test5");
-            fail("Expected IllegalArgumentException");
-        } catch (MissingResourceException e) {
-        }
+	}
 
-    }
+	public void testSerialisationException() throws Exception {
 
-    public void testNullLocale() throws Exception {
-        // resource exists, but locale is null
-        try {
-            new Resources("resources.test1");
-            fail("Expected NullPointerException");
-        } catch (NullPointerException e) {
-        }
-    }
+		try {
+			new Resources("resources.test4");
+			fail("Expected SerialisationException");
+		} catch (SerializationException e) {
+		}
 
-    public void testNullBaseName() throws Exception {
-        try {
-            new Resources(null);
-            fail("Expected NullPointerException");
-        } catch (NullPointerException e) {
-        }
-    }
+	}
 
-    @SuppressWarnings("unchecked")
-    private static void assertResources(Resources res, String acronym,
-            String term) {
-        assertTrue(res.containsKey("glossary"));
+	public void testMissingResource() throws Exception {
 
-        Map<String, Object> glossary = (Map<String, Object>) res
-                .get("glossary");
-        assertNotNull(glossary);
-        assertTrue(glossary.containsKey("GlossDiv"));
+		// resource doesn't exist...
+		try {
+			new Resources("resources.test5");
+			fail("Expected IllegalArgumentException");
+		} catch (MissingResourceException e) {
+		}
 
-        Map<String, Object> glossDiv = (Map<String, Object>) glossary
-                .get("GlossDiv");
-        assertNotNull(glossDiv);
+	}
 
-        assertEquals("S", glossDiv.get("title"));
+	public void testNullLocale() throws Exception {
+		// resource exists, but locale is null
+		try {
+			new Resources("resources.test1", (Locale) null);
+			fail("Expected IllegalArgumentException");
+		} catch (IllegalArgumentException e) {
 
-        assertTrue(glossDiv.containsKey("GlossList"));
-        Map<String, Object> glossList = (Map<String, Object>) glossDiv
-                .get("GlossList");
-        assertNotNull(glossList);
+		}
+	}
 
-        assertTrue(glossList.containsKey("GlossEntry"));
-        Map<String, Object> glossEntry = (Map<String, Object>) glossList
-                .get("GlossEntry");
-        assertNotNull(glossEntry);
+	public void testNullBaseName() throws Exception {
+		try {
+			new Resources(null);
+			fail("Expected NullPointerException");
+		} catch (NullPointerException e) {
+		}
+	}
 
-        assertEquals(acronym, glossEntry.get("Acronym"));
-        assertEquals(term, glossEntry.get("GlossTerm"));
+	@SuppressWarnings("unchecked")
+	private static void assertResources(Resources res, String acronym,
+			String term) {
+		assertTrue(res.containsKey("glossary"));
 
-        assertTrue(glossEntry.containsKey("GlossDef"));
-        Map<String, Object> glossDef = (Map<String, Object>) glossEntry
-                .get("GlossDef");
-        assertNotNull(glossDef);
+		Map<String, Object> glossary = (Map<String, Object>) res
+				.get("glossary");
+		assertNotNull(glossary);
+		assertTrue(glossary.containsKey("GlossDiv"));
 
-        assertTrue(glossDef.containsKey("GlossSeeAlso"));
-        List<String> list = (List<String>) glossDef.get("GlossSeeAlso");
-        assertNotNull(list);
-        assertEquals(2, list.getLength());
-    }
+		Map<String, Object> glossDiv = (Map<String, Object>) glossary
+				.get("GlossDiv");
+		assertNotNull(glossDiv);
+
+		assertEquals("S", glossDiv.get("title"));
+
+		assertTrue(glossDiv.containsKey("GlossList"));
+		Map<String, Object> glossList = (Map<String, Object>) glossDiv
+				.get("GlossList");
+		assertNotNull(glossList);
+
+		assertTrue(glossList.containsKey("GlossEntry"));
+		Map<String, Object> glossEntry = (Map<String, Object>) glossList
+				.get("GlossEntry");
+		assertNotNull(glossEntry);
+
+		assertEquals(acronym, glossEntry.get("Acronym"));
+		assertEquals(term, glossEntry.get("GlossTerm"));
+
+		assertTrue(glossEntry.containsKey("GlossDef"));
+		Map<String, Object> glossDef = (Map<String, Object>) glossEntry
+				.get("GlossDef");
+		assertNotNull(glossDef);
+
+		assertTrue(glossDef.containsKey("GlossSeeAlso"));
+		List<String> list = (List<String>) glossDef.get("GlossSeeAlso");
+		assertNotNull(list);
+		assertEquals(2, list.getLength());
+	}
 
 }
