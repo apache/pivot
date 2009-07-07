7e51cd0258f7fc4ab25ec2cc7c5cece1aa3e6b08
Todd Volkert <tvolkert@apache.org>
2009-07-07 11:04:22 +0000

    Added mutator methods to BasicAuthentication
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@791793 13f79535-47bb-0310-9956-ffa450edef68

--- web/src/org/apache/pivot/web/BasicAuthentication.java
+++ web/src/org/apache/pivot/web/BasicAuthentication.java
@@ -24,20 +24,28 @@ import org.apache.pivot.util.Base64;
  * Authentication</a> scheme.
  */
 public class BasicAuthentication implements Authentication {
-    private String username = null;
-    private String password = null;
+    private String username;
+    private String password;
 
     public BasicAuthentication(String username, String password) {
-        this.username = (username == null) ? "" : username;
-        this.password = (password == null) ? "" : password;
+        setUsername(username);
+        setPassword(password);
     }
 
     public String getUsername() {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = (username == null) ? "" : username;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = (password == null) ? "" : password;
     }
 
     public void authenticate(Query<?> query) {
