79feb89f13906baa298cf2d509455166a24dc1cb
Gregory K. Brown <gbrown@apache.org>
2009-07-22 12:53:09 +0000

    Add some test code.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@796691 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wtk/test/org/apache/pivot/wtk/test/ApplicationHandlerTest.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.wtk.test;
+
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Keyboard;
+import org.apache.pivot.wtk.Window;
+import org.apache.pivot.wtkx.WTKXSerializer;
+
+public class ApplicationHandlerTest implements Application,
+    Application.UnprocessedKeyHandler, Application.UncaughtExceptionHandler {
+    private Window window = null;
+
+    @Override
+    public void startup(Display display, Map<String, String> properties) throws Exception {
+        WTKXSerializer wtkxSerializer = new WTKXSerializer();
+        window = (Window)wtkxSerializer.readObject(this, "application_handler_test.wtkx");
+        window.open(display);
+    }
+
+    @Override
+    public boolean shutdown(boolean optional) {
+        if (window != null) {
+            window.close();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void suspend() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void keyTyped(char character) {
+        System.out.println("Unprocessed key typed: " + character);
+
+        if (character == 'a') {
+            throw new RuntimeException("'a' typed");
+        }
+    }
+
+    @Override
+    public void keyPressed(int keyCode, Keyboard.KeyLocation keyLocation) {
+        System.out.println("Unprocessed key pressed: " + keyCode + "; " + keyLocation);
+    }
+
+    @Override
+    public void keyReleased(int keyCode, Keyboard.KeyLocation keyLocation) {
+        System.out.println("Unprocessed key released: " + keyCode + "; " + keyLocation);
+    }
+
+    @Override
+    public void uncaughtExceptionThrown(Exception exception) {
+        exception.printStackTrace();
+    }
+
+    public static void main(String[] args) {
+        DesktopApplicationContext.main(ApplicationHandlerTest.class, args);
+    }
+}
---
+++ wtk/test/org/apache/pivot/wtk/test/ResolveTest.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.wtk.test;
+
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Window;
+import org.apache.pivot.wtkx.WTKXSerializer;
+
+public class ResolveTest implements Application {
+    private Window window = null;
+
+    @Override
+    public void startup(Display display, Map<String, String> properties) throws Exception {
+        Resources resources = new Resources(this);
+        WTKXSerializer wtkxSerializer = new WTKXSerializer(resources);
+        window = (Window)wtkxSerializer.readObject(this, "resolve_test.wtkx");
+        window.open(display);
+    }
+
+    @Override
+    public boolean shutdown(boolean optional) {
+        if (window != null) {
+            window.close();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void suspend() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    public static void main(String[] args) {
+        DesktopApplicationContext.main(ResolveTest.class, args);
+    }
+}
