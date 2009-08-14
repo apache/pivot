f60dbfe029b6209a5e737c285125db11c6581704
Gregory K. Brown <gbrown@apache.org>
2009-08-14 00:05:29 +0000

    Move file renderers into TerraFileBrowserSkin class.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@804062 13f79535-47bb-0310-9956-ffa450edef68

--- wtk/src/org/apache/pivot/wtk/skin/terra/TerraFileBrowserSkin.java
+++ wtk/src/org/apache/pivot/wtk/skin/terra/TerraFileBrowserSkin.java
@@ -16,8 +16,13 @@
  */
 package org.apache.pivot.wtk.skin.terra;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Date;
 
 import org.apache.pivot.collections.ArrayList;
 import org.apache.pivot.collections.Sequence;
@@ -25,18 +30,27 @@ import org.apache.pivot.io.Folder;
 import org.apache.pivot.serialization.SerializationException;
 import org.apache.pivot.util.Filter;
 import org.apache.pivot.util.Resources;
+import org.apache.pivot.util.concurrent.TaskExecutionException;
+import org.apache.pivot.wtk.BoxPane;
 import org.apache.pivot.wtk.Button;
 import org.apache.pivot.wtk.ButtonPressListener;
 import org.apache.pivot.wtk.Component;
 import org.apache.pivot.wtk.ComponentMouseButtonListener;
 import org.apache.pivot.wtk.Dimensions;
 import org.apache.pivot.wtk.FileBrowser;
+import org.apache.pivot.wtk.HorizontalAlignment;
+import org.apache.pivot.wtk.ImageView;
+import org.apache.pivot.wtk.Insets;
+import org.apache.pivot.wtk.Label;
 import org.apache.pivot.wtk.ListButton;
 import org.apache.pivot.wtk.ListButtonSelectionListener;
+import org.apache.pivot.wtk.ListView;
 import org.apache.pivot.wtk.Mouse;
 import org.apache.pivot.wtk.PushButton;
 import org.apache.pivot.wtk.ScrollPane;
 import org.apache.pivot.wtk.TableView;
+import org.apache.pivot.wtk.VerticalAlignment;
+import org.apache.pivot.wtk.media.Image;
 import org.apache.pivot.wtk.skin.FileBrowserSkin;
 import org.apache.pivot.wtkx.WTKX;
 import org.apache.pivot.wtkx.WTKXSerializer;
@@ -47,6 +61,260 @@ import org.apache.pivot.wtkx.WTKXSerializer;
  * @author gbrown
  */
 public class TerraFileBrowserSkin extends FileBrowserSkin {
+    /**
+     * Abstract renderer for displaying file system contents.
+     *
+     * @author gbrown
+     */
+    public static abstract class FileRenderer extends BoxPane {
+        protected ImageView imageView = new ImageView();
+        protected Label label = new Label();
+
+        public static final int ICON_WIDTH = 16;
+        public static final int ICON_HEIGHT = 16;
+
+        public static final Image FOLDER_IMAGE;
+        public static final Image HOME_FOLDER_IMAGE;
+        public static final Image FILE_IMAGE;
+
+        public static final int KILOBYTE = 1024;
+        public static final String[] ABBREVIATIONS = {"K", "M", "G", "T", "P", "E", "Z", "Y"};
+
+        public static final File HOME_DIRECTORY;
+
+        static {
+            try {
+                FOLDER_IMAGE = Image.load(FileRenderer.class.getResource("folder.png"));
+                HOME_FOLDER_IMAGE = Image.load(FileRenderer.class.getResource("folder_home.png"));
+                FILE_IMAGE = Image.load(FileRenderer.class.getResource("page_white.png"));
+
+                HOME_DIRECTORY = new File(System.getProperty("user.home"));
+            } catch (TaskExecutionException exception) {
+                throw new RuntimeException(exception);
+            }
+        }
+
+        public FileRenderer() {
+            getStyles().put("verticalAlignment", VerticalAlignment.CENTER);
+
+            add(imageView);
+            add(label);
+
+            imageView.setPreferredSize(ICON_WIDTH, ICON_HEIGHT);
+            imageView.getStyles().put("backgroundColor", null);
+        }
+
+        public void setSize(int width, int height) {
+            super.setSize(width, height);
+
+            // Since this component doesn't have a parent, it won't be validated
+            // via layout; ensure that it is valid here
+            validate();
+        }
+
+        /**
+         * Obtains the icon to display for a given file.
+         *
+         * @param file
+         */
+        public static Image getIcon(File file) {
+            Image icon;
+            if (file.isDirectory()) {
+                icon = file.equals(HOME_DIRECTORY) ? HOME_FOLDER_IMAGE : FOLDER_IMAGE;
+            } else {
+                icon = FILE_IMAGE;
+            }
+
+            return icon;
+        }
+
+        /**
+         * Converts a file size into a human-readable representation using binary
+         * prefixes (1KB = 1024 bytes).
+         *
+         * @param length
+         * The length of the file, in bytes. May be <tt>-1</tt> to indicate an
+         * unknown file size.
+         *
+         * @return
+         * The formatted file size, or null if <tt>length</tt> is <tt>-1</tt>.
+         */
+        public static String formatSize(File file) {
+            String formattedSize;
+
+            long length = file.length();
+            if (length == -1) {
+                formattedSize = null;
+            } else {
+                double size = length;
+
+                int i = -1;
+                do {
+                    size /= KILOBYTE;
+                    i++;
+                } while (size > KILOBYTE);
+
+                NumberFormat numberFormat = NumberFormat.getNumberInstance();
+                if (i == 0
+                    && size > 1) {
+                    numberFormat.setMaximumFractionDigits(0);
+                } else {
+                    numberFormat.setMaximumFractionDigits(1);
+                }
+
+                formattedSize = numberFormat.format(size) + " " + ABBREVIATIONS[i] + "B";
+            }
+
+            return formattedSize;
+        }
+    }
+
+    /**
+     * List button file renderer.
+     *
+     * @author gbrown
+     */
+    public static class ListButtonFileRenderer extends FileRenderer implements Button.DataRenderer {
+        public ListButtonFileRenderer() {
+            getStyles().put("horizontalAlignment", HorizontalAlignment.LEFT);
+        }
+
+        @Override
+        public void render(Object data, Button button, boolean highlight) {
+            if (data != null) {
+                File file = (File)data;
+
+                // Update the image view
+                imageView.setImage(getIcon(file));
+                imageView.getStyles().put("opacity", button.isEnabled() ? 1.0f : 0.5f);
+
+                // Update the label
+                String text = file.getName();
+                if (text.length() == 0) {
+                    text = System.getProperty("file.separator");
+                }
+
+                label.setText(text);
+            }
+        }
+    }
+
+    /**
+     * List view renderer for displaying file system contents.
+     *
+     * @author gbrown
+     */
+    public static class ListViewFileRenderer extends FileRenderer implements ListView.ItemRenderer {
+        public ListViewFileRenderer() {
+            getStyles().put("horizontalAlignment", HorizontalAlignment.LEFT);
+            getStyles().put("padding", new Insets(2, 3, 2, 3));
+        }
+
+        public void render(Object item, ListView listView, boolean selected,
+            boolean checked, boolean highlighted, boolean disabled) {
+            label.getStyles().put("font", listView.getStyles().get("font"));
+
+            Object color = null;
+            if (listView.isEnabled() && !disabled) {
+                if (selected) {
+                    if (listView.isFocused()) {
+                        color = listView.getStyles().get("selectionColor");
+                    } else {
+                        color = listView.getStyles().get("inactiveSelectionColor");
+                    }
+                } else {
+                    color = listView.getStyles().get("color");
+                }
+            } else {
+                color = listView.getStyles().get("disabledColor");
+            }
+
+            label.getStyles().put("color", color);
+
+            if (item != null) {
+                File file = (File)item;
+
+                // Update the image view
+                imageView.setImage(getIcon(file));
+                imageView.getStyles().put("opacity",
+                    (listView.isEnabled() && !disabled) ? 1.0f : 0.5f);
+
+                // Update the label
+                String text = file.getName();
+                if (text.length() == 0) {
+                    text = System.getProperty("file.separator");
+                }
+
+                label.setText(text);
+            }
+        }
+    }
+
+    public static class TableViewFileRenderer extends FileRenderer implements TableView.CellRenderer {
+        public static final String NAME_KEY = "name";
+        public static final String SIZE_KEY = "size";
+        public static final String LAST_MODIFIED_KEY = "lastModified";
+
+        public TableViewFileRenderer() {
+            getStyles().put("horizontalAlignment", HorizontalAlignment.CENTER);
+            getStyles().put("padding", new Insets(2));
+        }
+
+        public void render(Object value, TableView tableView, TableView.Column column,
+            boolean rowSelected, boolean rowHighlighted, boolean rowDisabled) {
+            String columnName = column.getName();
+
+            if (value != null) {
+                File file = (File)value;
+
+                String text = null;
+                Image icon = null;
+
+                if (columnName.equals(NAME_KEY)) {
+                    text = file.getName();
+                    icon = getIcon(file);
+                    getStyles().put("horizontalAlignment", HorizontalAlignment.LEFT);
+                } else if (columnName.equals(SIZE_KEY)) {
+                    text = formatSize(file);
+                    getStyles().put("horizontalAlignment", HorizontalAlignment.RIGHT);
+                } else if (columnName.equals(LAST_MODIFIED_KEY)) {
+                    long lastModified = file.lastModified();
+                    Date lastModifiedDate = new Date(lastModified);
+
+                    DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+                    text = dateFormat.format(lastModifiedDate);
+                    getStyles().put("horizontalAlignment", HorizontalAlignment.RIGHT);
+                } else {
+                    System.err.println("Unexpected column name in " + getClass().getName()
+                        + ": " + columnName);
+                }
+
+                label.setText(text);
+                imageView.setImage(icon);
+            }
+
+            Font font = (Font)tableView.getStyles().get("font");
+            label.getStyles().put("font", font);
+
+            Color color;
+            if (tableView.isEnabled() && !rowDisabled) {
+                if (rowSelected) {
+                    if (tableView.isFocused()) {
+                        color = (Color)tableView.getStyles().get("selectionColor");
+                    } else {
+                        color = (Color)tableView.getStyles().get("inactiveSelectionColor");
+                    }
+                } else {
+                    color = (Color)tableView.getStyles().get("color");
+                }
+            } else {
+                color = (Color)tableView.getStyles().get("disabledColor");
+            }
+
+            label.getStyles().put("color", color);
+        }
+    }
+
     private Component content = null;
 
     @WTKX private ListButton pathListButton = null;
