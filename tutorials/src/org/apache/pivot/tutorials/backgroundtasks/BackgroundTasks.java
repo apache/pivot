23c6b017ad132c5808bbd8d84c5ab88949ef9519
Gregory K. Brown <gbrown@apache.org>
2009-09-30 18:36:17 +0000

    Add background task tutorial.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@820378 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tutorials/src/org/apache/pivot/tutorials/backgroundtasks/BackgroundTasks.java
@@ -0,0 +1,127 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.tutorials.backgroundtasks;
+
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.concurrent.Task;
+import org.apache.pivot.util.concurrent.TaskExecutionException;
+import org.apache.pivot.util.concurrent.TaskListener;
+import org.apache.pivot.wtk.ActivityIndicator;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.Button;
+import org.apache.pivot.wtk.ButtonPressListener;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.PushButton;
+import org.apache.pivot.wtk.TaskAdapter;
+import org.apache.pivot.wtk.Window;
+import org.apache.pivot.wtkx.WTKXSerializer;
+
+public class BackgroundTasks implements Application {
+    private Window window = null;
+
+    private ActivityIndicator activityIndicator = null;
+    private PushButton executeSynchronousButton = null;
+    private PushButton executeAsynchronousButton = null;
+
+    @Override
+    public void startup(Display display, Map<String, String> properties) throws Exception {
+        WTKXSerializer wtkxSerializer = new WTKXSerializer();
+        window = (Window)wtkxSerializer.readObject(this, "background_tasks.wtkx");
+
+        activityIndicator = (ActivityIndicator)wtkxSerializer.get("activityIndicator");
+
+        executeSynchronousButton = (PushButton)wtkxSerializer.get("executeSynchronousButton");
+        executeSynchronousButton.getButtonPressListeners().add(new ButtonPressListener() {
+            @Override
+            public void buttonPressed(Button button) {
+                activityIndicator.setActive(true);
+
+                System.out.println("Starting synchronous task execution.");
+
+                SleepTask sleepTask = new SleepTask();
+
+                String result = null;
+                try {
+                    result = sleepTask.execute();
+                } catch (TaskExecutionException exception) {
+                    System.err.println(exception);
+                }
+
+                System.out.println("Synchronous task execution complete: \"" + result + "\"");
+
+                activityIndicator.setActive(false);
+            }
+        });
+
+        executeAsynchronousButton = (PushButton)wtkxSerializer.get("executeAsynchronousButton");
+        executeAsynchronousButton.getButtonPressListeners().add(new ButtonPressListener() {
+            @Override
+            public void buttonPressed(Button button) {
+                activityIndicator.setActive(true);
+                window.setEnabled(false);
+
+                System.out.println("Starting asynchronous task execution.");
+
+                SleepTask sleepTask = new SleepTask();
+                TaskListener<String> taskListener = new TaskListener<String>() {
+                    @Override
+                    public void taskExecuted(Task<String> task) {
+                        activityIndicator.setActive(false);
+                        window.setEnabled(true);
+
+                        System.out.println("Synchronous task execution complete: \""
+                            + task.getResult() + "\"");
+                    }
+
+                    @Override
+                    public void executeFailed(Task<String> task) {
+                        activityIndicator.setActive(false);
+                        window.setEnabled(true);
+
+                        System.err.println(task.getFault());
+                    }
+                };
+
+                sleepTask.execute(new TaskAdapter<String>(taskListener));
+            }
+        });
+
+        window.open(display);
+    }
+
+    @Override
+    public boolean shutdown(boolean optional) {
+        if (window != null) {
+            window.close();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void suspend() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    public static void main(String[] args) {
+        DesktopApplicationContext.main(BackgroundTasks.class, args);
+    }
+}
