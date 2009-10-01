08be0e7d7291f02dadd3190287436b749f029037
Gregory K. Brown <gbrown@apache.org>
2009-10-01 16:52:16 +0000

    Add a web query tutorial.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@820716 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tutorials/src/org/apache/pivot/tutorials/webqueries/ResultItemRenderer.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.tutorials.webqueries;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.wtk.BoxPane;
+import org.apache.pivot.wtk.Insets;
+import org.apache.pivot.wtk.Label;
+import org.apache.pivot.wtk.ListView;
+import org.apache.pivot.wtk.Orientation;
+
+public class ResultItemRenderer extends BoxPane implements ListView.ItemRenderer {
+    private Label titleLabel = new Label();
+    private Label addressLabel = new Label();
+    private Label phoneLabel = new Label();
+
+    public ResultItemRenderer() {
+        super(Orientation.VERTICAL);
+
+        add(titleLabel);
+        add(addressLabel);
+        add(phoneLabel);
+
+        getStyles().put("padding", new Insets(3, 2, 3, 2));
+        getStyles().put("spacing", 2);
+    }
+
+    @Override
+    public void setSize(int width, int height) {
+        super.setSize(width, height);
+        validate();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void render(Object item, int index, ListView listView, boolean selected,
+        boolean checked, boolean highlighted, boolean disabled) {
+        if (item != null) {
+            Map<String, Object> map = (Map<String, Object>)item;
+
+            titleLabel.setText((String)map.get("title"));
+            phoneLabel.setText((String)map.get("Phone"));
+
+            Map<String, Object> location = (Map<String, Object>)map.get("y:location");
+            if (location == null) {
+                addressLabel.setText(null);
+            } else {
+                String street = (String)location.get("street");
+                String city = (String)location.get("city");
+                String state = (String)location.get("state");
+                addressLabel.setText(street + ", " + city + " " + state);
+            }
+        }
+
+        Font font = (Font)listView.getStyles().get("font");
+        titleLabel.getStyles().put("font", font);
+        titleLabel.getStyles().put("fontBold", true);
+        phoneLabel.getStyles().put("font", font);
+        addressLabel.getStyles().put("font", font);
+
+        Color color;
+        if (listView.isEnabled() && !disabled) {
+            if (selected) {
+                if (listView.isFocused()) {
+                    color = (Color)listView.getStyles().get("selectionColor");
+                } else {
+                    color = (Color)listView.getStyles().get("inactiveSelectionColor");
+                }
+            } else {
+                color = (Color)listView.getStyles().get("color");
+            }
+        } else {
+            color = (Color)listView.getStyles().get("disabledColor");
+        }
+
+        titleLabel.getStyles().put("color", color);
+        phoneLabel.getStyles().put("color", color);
+        addressLabel.getStyles().put("color", color);
+    }
+}
---
+++ tutorials/src/org/apache/pivot/tutorials/webqueries/WebQueries.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.tutorials.webqueries;
+
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.serialization.JSONSerializer;
+import org.apache.pivot.util.concurrent.Task;
+import org.apache.pivot.util.concurrent.TaskListener;
+import org.apache.pivot.web.GetQuery;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Label;
+import org.apache.pivot.wtk.ListView;
+import org.apache.pivot.wtk.TaskAdapter;
+import org.apache.pivot.wtk.Window;
+import org.apache.pivot.wtkx.WTKXSerializer;
+
+public class WebQueries implements Application {
+    private Window window = null;
+
+    private ListView listView = null;
+    private Label loadingLabel = null;
+
+    @Override
+    public void startup(Display display, Map<String, String> properties) throws Exception {
+        WTKXSerializer wtkxSerializer = new WTKXSerializer();
+        window = (Window)wtkxSerializer.readObject(this, "web_queries.wtkx");
+
+        listView = (ListView)wtkxSerializer.get("listView");
+        loadingLabel = (Label)wtkxSerializer.get("loadingLabel");
+
+        // Execute the query:
+        // http://pipes.yahoo.com/pipes/pipe.run?_id=43115761f2da5af5341ae2e56a93d646&_render=json
+        GetQuery getQuery = new GetQuery("pipes.yahoo.com", "/pipes/pipe.run");
+        getQuery.getParameters().put("_id", "43115761f2da5af5341ae2e56a93d646");
+        getQuery.getParameters().put("_render", "json");
+
+        getQuery.execute(new TaskAdapter<Object>(new TaskListener<Object>() {
+            @Override
+            public void taskExecuted(Task<Object> task) {
+                listView.setListData(JSONSerializer.getList(task.getResult(), "value.items"));
+                loadingLabel.setVisible(false);
+            }
+
+            @Override
+            public void executeFailed(Task<Object> task) {
+                loadingLabel.setText(task.getFault().getMessage());
+            }
+        }));
+
+        window.open(display);
+    }
+
+    @Override
+    public boolean shutdown(boolean optional) {
+        if (window != null) {
+            window.close();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void suspend() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    public static void main(String[] args) {
+        DesktopApplicationContext.main(WebQueries.class, args);
+    }
+}
