b3bd7668ac613517120ea2e6f72cf932cd3de6f4
Todd Volkert <tvolkert@apache.org>
2009-10-27 16:09:22 +0000

    Updates to component explorer demo, updated build file to make demos project depend on tools
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@830232 13f79535-47bb-0310-9956-ffa450edef68

---
+++ demos/src/org/apache/pivot/demos/explorer/ComponentExplorer.java
@@ -0,0 +1,136 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.demos.explorer;
+
+import java.io.IOException;
+
+import org.apache.pivot.collections.List;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.collections.Sequence;
+import org.apache.pivot.collections.Sequence.Tree.Path;
+import org.apache.pivot.serialization.SerializationException;
+import org.apache.pivot.tools.wtk.ComponentInspector;
+import org.apache.pivot.tools.wtk.EventLogger;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.Component;
+import org.apache.pivot.wtk.ComponentMouseButtonListener;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Mouse;
+import org.apache.pivot.wtk.ScrollPane;
+import org.apache.pivot.wtk.TreeView;
+import org.apache.pivot.wtk.TreeViewSelectionListener;
+import org.apache.pivot.wtk.Window;
+import org.apache.pivot.wtkx.WTKXSerializer;
+
+public class ComponentExplorer implements Application {
+    private Window window = null;
+    private TreeView treeView = null;
+    private ScrollPane scrollPane = null;
+    private ComponentInspector componentInspector = null;
+    private EventLogger eventLogger = null;
+
+    @Override
+    @SuppressWarnings({"unchecked"})
+    public void startup(Display display, Map<String, String> properties)
+        throws Exception {
+        WTKXSerializer wtkxSerializer = new WTKXSerializer();
+        window = (Window)wtkxSerializer.readObject(this, "component_explorer.wtkx");
+
+        treeView = wtkxSerializer.getValue("treeView");
+        scrollPane = wtkxSerializer.getValue("scrollPane");
+        componentInspector = wtkxSerializer.getValue("componentInspector");
+        eventLogger = wtkxSerializer.getValue("eventLogger");
+
+        treeView.getTreeViewSelectionListeners().add(new TreeViewSelectionListener.Adapter() {
+            @Override
+            public void selectedPathsChanged(TreeView treeView,
+                Sequence<Path> previousSelectedPaths) {
+                Component component = null;
+
+                Object node = treeView.getSelectedNode();
+                if (node instanceof ComponentNode) {
+                    ComponentNode componentNode = (ComponentNode)node;
+
+                    WTKXSerializer wtkxSerializer = new WTKXSerializer();
+                    try {
+                        component = (Component)wtkxSerializer.readObject(componentNode.getSrc());
+                    } catch (IOException exception) {
+                        throw new RuntimeException(exception);
+                    } catch (SerializationException exception) {
+                        throw new RuntimeException(exception);
+                    }
+
+                    scrollPane.setHorizontalScrollBarPolicy
+                        (componentNode.getHorizontalScrollBarPolicy());
+                    scrollPane.setVerticalScrollBarPolicy
+                        (componentNode.getVerticalScrollBarPolicy());
+                }
+
+                scrollPane.setView(component);
+                componentInspector.setSource(component);
+                eventLogger.setSource(component);
+            }
+        });
+
+        treeView.getComponentMouseButtonListeners().add(new ComponentMouseButtonListener.Adapter() {
+            @Override
+            public boolean mouseClick(Component component, Mouse.Button button, int x, int y,
+                int count) {
+                if (button == Mouse.Button.LEFT && count == 2) {
+                    Path path = treeView.getNodeAt(y);
+
+                    if (path != null) {
+                        List<?> treeData = treeView.getTreeData();
+                        Object treeNode = Sequence.Tree.get(treeData, path);
+
+                        if (treeNode instanceof List<?>) {
+                            treeView.setBranchExpanded(path, !treeView.isBranchExpanded(path));
+                        }
+                    }
+                }
+
+                return false;
+            }
+        });
+
+        treeView.expandAll();
+
+        window.open(display);
+    }
+
+    @Override
+    public boolean shutdown(boolean optional) throws Exception {
+        if (window != null) {
+            window.close();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void suspend() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    public static void main(String[] args) {
+        DesktopApplicationContext.main(ComponentExplorer.class, args);
+    }
+}
