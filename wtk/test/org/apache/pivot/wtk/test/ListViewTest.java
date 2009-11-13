a28b9d69bb7049a86a1ed650a14c395baefe2796
Noel John Grandin <noelgrandin@apache.org>
2009-11-13 09:55:45 +0000

    Add test code for ListView, including new variableItemHeight property
    git-svn-id: https://svn.apache.org/repos/asf/incubator/pivot/trunk@835794 13f79535-47bb-0310-9956-ffa450edef68

--- wtk/test/org/apache/pivot/wtk/test/ListViewTest.java
+++ wtk/test/org/apache/pivot/wtk/test/ListViewTest.java
@@ -58,7 +58,6 @@ public class ListViewTest implements Application {
             }
         });
 
-        listView.setCheckmarksEnabled(true);
         listView.setItemChecked(4, true);
         listView.setItemChecked(6, true);
 
