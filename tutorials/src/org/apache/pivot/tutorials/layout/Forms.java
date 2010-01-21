c202e19f42b61e082acea7684189e5f37915327b
Gregory K. Brown <gbrown@apache.org>
2010-01-21 17:00:30 +0000

    Revert accidental update to Forms tutorial; update Stock Tracker for recent changes to TerraFormSkin.
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@901774 13f79535-47bb-0310-9956-ffa450edef68

--- tutorials/src/org/apache/pivot/tutorials/layout/Forms.java
+++ tutorials/src/org/apache/pivot/tutorials/layout/Forms.java
@@ -35,8 +35,6 @@ import org.apache.pivot.wtkx.WTKXSerializer;
 public class Forms implements Application {
     private Window window = null;
     private BoxPane nameBoxPane = null;
-    private BoxPane homeAddressBoxPane = null;
-    private BoxPane workAddressBoxPane = null;
     private TextInput lastNameTextInput = null;
     private TextInput firstNameTextInput = null;
     private PushButton submitButton = null;
@@ -47,8 +45,6 @@ public class Forms implements Application {
         WTKXSerializer wtkxSerializer = new WTKXSerializer();
         window = (Window)wtkxSerializer.readObject(this, "forms.wtkx");
         nameBoxPane = (BoxPane)wtkxSerializer.get("nameBoxPane");
-        homeAddressBoxPane = (BoxPane)wtkxSerializer.get("homeAddressBoxPane");
-        workAddressBoxPane = (BoxPane)wtkxSerializer.get("workAddressBoxPane");
         lastNameTextInput = (TextInput)wtkxSerializer.get("lastNameTextInput");
         firstNameTextInput = (TextInput)wtkxSerializer.get("firstNameTextInput");
         submitButton = (PushButton)wtkxSerializer.get("submitButton");
@@ -67,8 +63,6 @@ public class Forms implements Application {
                 }
 
                 Form.setFlag(nameBoxPane, flag);
-                Form.setFlag(homeAddressBoxPane, new Form.Flag(MessageType.WARNING, "This is a warning."));
-                Form.setFlag(workAddressBoxPane, new Form.Flag(MessageType.WARNING, "This is also a warning."));
 
                 if (flag == null) {
                     errorLabel.setText(null);
