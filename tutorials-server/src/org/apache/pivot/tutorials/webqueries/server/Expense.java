470fc60428eb742d3eaf80cb48b7ecd5832bc9ce
Gregory K. Brown <gbrown@apache.org>
2010-06-08 12:04:48 +0000

    Resolve PIVOT-521; continue development of web query server tutorial; update sample dates in row editor demo to be more current.
    git-svn-id: https://svn.apache.org/repos/asf/pivot/branches/1.5.x@952626 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tutorials-server/src/org/apache/pivot/tutorials/webqueries/server/Expense.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.tutorials.webqueries.server;
+
+import org.apache.pivot.util.CalendarDate;
+
+public class Expense {
+    private int id;
+
+    private CalendarDate calendarDate = null;
+    private String type = null;
+    private double amount = 0;
+    private String description = null;
+
+    private static int NEXT_ID = 0;
+
+    public Expense() {
+        id = NEXT_ID++;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public CalendarDate getDate() {
+        return calendarDate;
+    }
+
+    public void setDate(CalendarDate calendarDate) {
+        this.calendarDate = calendarDate;
+    }
+
+    public final void setDate(String calendarDate) {
+        setDate(CalendarDate.decode(calendarDate));
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public final void setAmount(String amount) {
+        setAmount(Double.parseDouble(amount));
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
