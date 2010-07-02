62fd0baf8896fc6e4391fb92f29e29b49419e819
Gregory K. Brown <gbrown@apache.org>
2010-07-02 17:08:54 +0000

    Fix rounded rectangle rendering issue in buttons.
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@960059 13f79535-47bb-0310-9956-ffa450edef68

--- wtk-terra/src/org/apache/pivot/wtk/skin/terra/TerraCalendarButtonSkin.java
+++ wtk-terra/src/org/apache/pivot/wtk/skin/terra/TerraCalendarButtonSkin.java
@@ -236,7 +236,7 @@ public class TerraCalendarButtonSkin extends CalendarButtonSkin {
         // Paint the background
         graphics.setPaint(new GradientPaint(width / 2f, 0, bevelColor,
             width / 2f, height / 2f, backgroundColor));
-        graphics.fill(new RoundRectangle2D.Double(0, 0, width, height,
+        graphics.fill(new RoundRectangle2D.Double(0.5, 0.5, width - 1, height - 1,
             CORNER_RADIUS, CORNER_RADIUS));
 
         // Paint the content
