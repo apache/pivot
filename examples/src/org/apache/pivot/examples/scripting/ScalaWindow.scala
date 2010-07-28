3cf3e60d78fafe2e19f3770de727b1bc2aad2c60
Gregory K. Brown <gbrown@apache.org>
2010-07-28 12:23:18 +0000

    Add example of using Scala with Pivot; minor updates to Groovy and JavaScript examples.
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@980034 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/src/org/apache/pivot/examples/scripting/ScalaWindow.scala
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.examples.scripting;
+
+import java.net.URL;
+
+import org.apache.pivot.beans.Bindable;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.Alert;
+import org.apache.pivot.wtk.Button;
+import org.apache.pivot.wtk.ButtonPressListener;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.PushButton;
+import org.apache.pivot.wtk.Window;
+
+class ScalaWindow extends Window with Bindable {
+    private var sayHelloButton:PushButton = null;
+    
+    override def initialize(namespace: Map[String, Object], location:URL, resources:Resources) {
+        sayHelloButton = namespace.get("sayHelloButton").asInstanceOf[PushButton];
+        
+        sayHelloButton.getButtonPressListeners().add(new ButtonPressListener {
+            override def buttonPressed(button:Button) {
+                sayHello();
+            }
+        });
+    }
+    
+    override def open(display:Display, owner:Window) {
+        super.open(display, owner);
+
+        sayHelloButton.requestFocus();
+    }
+    
+    private def sayHello() {
+        Alert.alert("Hello from Scala!", this);
+    }
+}
