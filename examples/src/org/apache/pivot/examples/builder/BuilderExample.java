3e855f8f32b1ce0f36ee50d45d64f7d8855942e9
Gregory K. Brown <gbrown@apache.org>
2011-01-03 21:29:18 +0000

    Add builder example/comparison.
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1054762 13f79535-47bb-0310-9956-ffa450edef68

---
+++ examples/src/org/apache/pivot/examples/builder/BuilderExample.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.examples.builder;
+
+import java.io.IOException;
+
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.serialization.SerializationException;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Label;
+import org.apache.pivot.wtk.TabPane;
+import org.apache.pivot.wtk.Window;
+
+public class BuilderExample implements Application {
+    private Window window = null;
+
+    @Override
+    public void startup(Display display, Map<String, String> properties)
+        throws Exception {
+        window = buildWindow();
+        window.open(display);
+    }
+
+    @Override
+    public boolean shutdown(boolean optional) {
+        if (window != null) {
+            window.close();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void suspend() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    private Window buildWindow()
+        throws SerializationException, IOException {
+        return new Window() {
+            {   setContent(new TabPane() {
+                    {   getTabs().add(new Label() {
+                            {   setText("Label 1");
+                                TabPane.setTabData(this, "Label 1");
+                            }
+                        });
+
+                        getTabs().add(new Label() {
+                            {   setText("Label 2");
+                                TabPane.setTabData(this, "Label 2");
+                            }
+                        });
+
+                        getTabs().add(new Label() {
+                            {   setText("Label 3");
+                                TabPane.setTabData(this, "Label 3");
+                            }
+                        });
+
+                        setSelectedIndex(2);
+                    }
+                });
+
+                setTitle("Builder Example");
+                setMaximized(true);
+            }
+        };
+    }
+}
