7e9d1ecc3ae86c0443fe638f77ac57addbd0e958
Sandro Martini <smartini@apache.org>
2011-03-21 23:58:59 +0000

    PIVOT-686 : test case (at lest for the application use case)
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1084029 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/src/org/apache/pivot/tests/issues/Pivot686.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.tests.issues;
+
+import org.apache.pivot.beans.BXMLSerializer;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.Button;
+import org.apache.pivot.wtk.ButtonPressListener;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.PushButton;
+import org.apache.pivot.wtk.TextInput;
+import org.apache.pivot.wtk.Window;
+
+public class Pivot686 implements Application {
+    private Window window = null;
+    private TextInput textInput = null;
+    private PushButton pushButton = null;
+    
+    @Override
+    public void startup(Display display, Map<String, String> properties)
+        throws Exception {
+        System.out.println("startup: start");
+
+        BXMLSerializer bxmlSerializer = new BXMLSerializer();
+        window = (Window)bxmlSerializer.readObject(this.getClass(), "pivot_686.bxml");
+        initializeFields(bxmlSerializer);
+        window.open(display);
+
+        textInput.requestFocus();  // force focus on TextInput, Ok when run as a Java Application
+        System.out.println("textInput has focus: " + textInput.isFocused());
+        
+        System.out.println("startup: end");
+    }
+
+    @Override
+    public boolean shutdown(boolean optional) {
+        if (window != null) {
+            window.close();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void suspend() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    private void initializeFields(BXMLSerializer serializer) {
+        System.out.println("initializeFields: start");
+
+        textInput = (TextInput)serializer.getNamespace().get("textInput");
+        textInput.requestFocus();  // note that this has no effect here
+        System.out.println("textInput has focus: " + textInput.isFocused());
+
+        pushButton = (PushButton)serializer.getNamespace().get("pushButton");
+        pushButton.getButtonPressListeners().add(new ButtonPressListener() {
+            @Override
+            public void buttonPressed(Button button) {
+                String msg = "You clicked me!";
+                System.out.println(msg);
+                // Alert.alert(MessageType.INFO, msg, window);
+                
+                textInput.setText("");
+                textInput.requestFocus();
+            }
+        });
+        
+        System.out.println("initializeFields: end");
+    }
+
+    public static void main(String[] args) {
+        DesktopApplicationContext.main(Pivot686.class, args);
+    }
+
+}
