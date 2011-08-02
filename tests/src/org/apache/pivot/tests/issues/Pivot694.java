0ec93f2f319ee06a94f513baa76116967249183d
Sandro Martini <smartini@apache.org>
2011-08-02 00:21:59 +0000

    PIVOT-694 , Test application with many other components, and set some work for the future (clearSelection on more components)
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1152982 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/org/apache/pivot/tests/issues/Pivot694.java
+++ tests/src/org/apache/pivot/tests/issues/Pivot694.java
@@ -16,28 +16,50 @@
  */
 package org.apache.pivot.tests.issues;
 
+import java.util.Date;
+
 import org.apache.pivot.beans.BXMLSerializer;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.wtk.Application;
 import org.apache.pivot.wtk.Button;
+import org.apache.pivot.wtk.ButtonGroup;
 import org.apache.pivot.wtk.ButtonPressListener;
+import org.apache.pivot.wtk.CalendarButton;
 import org.apache.pivot.wtk.DesktopApplicationContext;
 import org.apache.pivot.wtk.Display;
 import org.apache.pivot.wtk.ListButton;
 import org.apache.pivot.wtk.PushButton;
+import org.apache.pivot.wtk.Spinner;
+import org.apache.pivot.wtk.TableView;
+import org.apache.pivot.wtk.TreeView;
 import org.apache.pivot.wtk.Window;
 
 public class Pivot694 implements Application {
     private Window window = null;
+    private ButtonGroup pushButtons = null;
+    private ButtonGroup toggleButtons = null;
+    private ButtonGroup radioButtons = null;
+    private ButtonGroup checkButtons = null;
+    private ButtonGroup checkTriButtons = null;
+
+    private CalendarButton calendarButton = null;
+    private Spinner spinner = null;
+
+    private TableView tableView = null;
+    private TreeView  treeView  = null;
+
     private ListButton listButton1 = null;
-    private PushButton pushButton1 = null;
     private ListButton listButton2 = null;
-    private PushButton pushButton2 = null;
+
+    private PushButton clearSelectionButton = null;
+    private PushButton clearButton = null;
+
 
     @Override
-    public void startup(Display display, Map<String, String> properties)
-        throws Exception {
+    public void startup(Display display, Map<String, String> properties) throws Exception {
         System.out.println("startup: start");
+
+        System.out.println("Test for clearing (selection) in many Components");
 
         BXMLSerializer bxmlSerializer = new BXMLSerializer();
         window = (Window)bxmlSerializer.readObject(this.getClass(), "pivot_694.bxml");
@@ -67,28 +89,73 @@ public class Pivot694 implements Application {
     private void initializeFields(BXMLSerializer serializer) {
         System.out.println("initializeFields: start");
 
+        pushButtons = (ButtonGroup)serializer.getNamespace().get("pushButtons");
+        toggleButtons = (ButtonGroup)serializer.getNamespace().get("toggleButtons");
+        radioButtons = (ButtonGroup)serializer.getNamespace().get("radioButtons");
+        checkButtons = (ButtonGroup)serializer.getNamespace().get("checkButtons");
+        checkTriButtons = (ButtonGroup)serializer.getNamespace().get("checkTriButtons");
+
+        calendarButton = (CalendarButton)serializer.getNamespace().get("calendarButton");
+        spinner = (Spinner)serializer.getNamespace().get("spinner");
+
+        tableView = (TableView)serializer.getNamespace().get("tableView");
+        treeView  = (TreeView)serializer.getNamespace().get("treeView");
+
         listButton1 = (ListButton)serializer.getNamespace().get("listButton1");
-        pushButton1 = (PushButton)serializer.getNamespace().get("pushButton1");
-        pushButton1.getButtonPressListeners().add(new ButtonPressListener() {
+        listButton2 = (ListButton)serializer.getNamespace().get("listButton2");
+
+        clearSelectionButton = (PushButton)serializer.getNamespace().get("clearSelectionButton");
+        clearSelectionButton.getButtonPressListeners().add(new ButtonPressListener() {
             @Override
             public void buttonPressed(Button button) {
-                System.out.println("Clearing selection from " + button.getName());
-                listButton1.clear();
+                System.out.println("Clearing selection from " + button.getName() + " at " + new Date());
+
+    //TODO: verify what to do on these ...
+//                pushButtons.clearSelection();
+//                toggleButtons.clearSelection();
+//                radioButtons.clearSelection();
+//                checkButtons.clearSelection();
+//                checkTriButtons.clearSelection();
+
+//                calendarButton.clearSelection();
+//                spinner.clearSelection();
+
+                tableView.clearSelection();
+                treeView.clearSelection();
+
+//              listButton1.clearSelection();
+//              listButton2.clearSelection();
+
             }
         });
 
-        listButton2 = (ListButton)serializer.getNamespace().get("listButton2");
-        pushButton2 = (PushButton)serializer.getNamespace().get("pushButton2");
-        pushButton2.getButtonPressListeners().add(new ButtonPressListener() {
+        clearButton = (PushButton)serializer.getNamespace().get("clearButton");
+        clearButton.getButtonPressListeners().add(new ButtonPressListener() {
             @Override
             public void buttonPressed(Button button) {
-                System.out.println("Clearing selection from " + button.getName());
+                System.out.println("Clearing data from " + button.getName() + " at " + new Date());
+
+// TODO: verify what to do on these ...
+//                pushButtons.remove(button);
+//                toggleButtons.remove(button);
+//                radioButtons.remove(button);
+//                checkButtons.remove(button);
+//                checkTriButtons.remove(button);
+
+                calendarButton.clear();
+                spinner.clear();
+
+                tableView.clear();
+                treeView.clear();
+
+                listButton1.clear();
                 listButton2.clear();
             }
         });
 
         System.out.println("initializeFields: end");
     }
+
 
     public static void main(String[] args) {
         DesktopApplicationContext.main(Pivot694.class, args);
