6c694bcbd03b4c67d5f3ca05b1ed4bc8ea817fc2
Sandro Martini <smartini@apache.org>
2011-08-04 01:27:39 +0000

    PIVOT-694 , Fix from Greg is Ok. But other problems are visible now (see some TODO inside Pivot694.java)
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1153719 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/org/apache/pivot/tests/issues/Pivot694.java
+++ tests/src/org/apache/pivot/tests/issues/Pivot694.java
@@ -22,12 +22,13 @@ import org.apache.pivot.beans.BXMLSerializer;
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.wtk.Application;
 import org.apache.pivot.wtk.Button;
-import org.apache.pivot.wtk.ButtonGroup;
 import org.apache.pivot.wtk.ButtonPressListener;
 import org.apache.pivot.wtk.CalendarButton;
+import org.apache.pivot.wtk.Component;
 import org.apache.pivot.wtk.DesktopApplicationContext;
 import org.apache.pivot.wtk.Display;
 import org.apache.pivot.wtk.ListButton;
+import org.apache.pivot.wtk.ListView;
 import org.apache.pivot.wtk.PushButton;
 import org.apache.pivot.wtk.Spinner;
 import org.apache.pivot.wtk.TableView;
@@ -36,17 +37,25 @@ import org.apache.pivot.wtk.Window;
 
 public class Pivot694 implements Application {
     private Window window = null;
-    private ButtonGroup pushButtons = null;
-    private ButtonGroup toggleButtons = null;
-    private ButtonGroup radioButtons = null;
-    private ButtonGroup checkButtons = null;
-    private ButtonGroup checkTriButtons = null;
 
-    private CalendarButton calendarButton = null;
-    private Spinner spinner = null;
+// TODO: for release 2.1 ... maybe generalizing some test here
+//    private ButtonGroup pushButtons = null;
+//    private ButtonGroup toggleButtons = null;
+//    private ButtonGroup radioButtons = null;
+//    private ButtonGroup checkButtons = null;
+//    private ButtonGroup checkTriButtons = null;
 
-    private TableView tableView = null;
-    private TreeView  treeView  = null;
+    private CalendarButton calendarButton1 = null;
+    private CalendarButton calendarButton2 = null;
+    private Spinner spinner1 = null;
+    private Spinner spinner2 = null;
+
+    private ListView  listView1 = null;
+    private ListView  listView2 = null;
+    private TableView tableView1 = null;
+    private TableView tableView2 = null;
+    private TreeView  treeView1 = null;
+    private TreeView  treeView2 = null;
 
     private ListButton listButton1 = null;
     private ListButton listButton2 = null;
@@ -65,6 +74,14 @@ public class Pivot694 implements Application {
         window = (Window)bxmlSerializer.readObject(this.getClass(), "pivot_694.bxml");
         initializeFields(bxmlSerializer);
         window.open(display);
+
+        System.out.println("Note that elements to clear selection, "
+            + " are to improve Pivot on this feature in a next release, so now only some components will support it"
+        );
+        System.out.println("Note that elements to clear (data) inside, will be emptied"
+            + " only if they have a buttonDataKey/listDataKey/tableDataKey/treeDataKey/spinnerDataKey/xxxDataKey"
+            + " property set at creation time"
+        );
 
         System.out.println("startup: end");
     }
@@ -89,17 +106,24 @@ public class Pivot694 implements Application {
     private void initializeFields(BXMLSerializer serializer) {
         System.out.println("initializeFields: start");
 
-        pushButtons = (ButtonGroup)serializer.getNamespace().get("pushButtons");
-        toggleButtons = (ButtonGroup)serializer.getNamespace().get("toggleButtons");
-        radioButtons = (ButtonGroup)serializer.getNamespace().get("radioButtons");
-        checkButtons = (ButtonGroup)serializer.getNamespace().get("checkButtons");
-        checkTriButtons = (ButtonGroup)serializer.getNamespace().get("checkTriButtons");
+// TODO: for release 2.1 ... maybe generalizing some test here
+//        pushButtons = (ButtonGroup)serializer.getNamespace().get("pushButtons");
+//        toggleButtons = (ButtonGroup)serializer.getNamespace().get("toggleButtons");
+//        radioButtons = (ButtonGroup)serializer.getNamespace().get("radioButtons");
+//        checkButtons = (ButtonGroup)serializer.getNamespace().get("checkButtons");
+//        checkTriButtons = (ButtonGroup)serializer.getNamespace().get("checkTriButtons");
 
-        calendarButton = (CalendarButton)serializer.getNamespace().get("calendarButton");
-        spinner = (Spinner)serializer.getNamespace().get("spinner");
+        calendarButton1 = (CalendarButton)serializer.getNamespace().get("calendarButton1");
+        calendarButton2 = (CalendarButton)serializer.getNamespace().get("calendarButton2");
+        spinner1 = (Spinner)serializer.getNamespace().get("spinner1");
+        spinner2 = (Spinner)serializer.getNamespace().get("spinner2");
 
-        tableView = (TableView)serializer.getNamespace().get("tableView");
-        treeView  = (TreeView)serializer.getNamespace().get("treeView");
+        listView1 = (ListView)serializer.getNamespace().get("listView1");
+        listView2 = (ListView)serializer.getNamespace().get("listView2");
+        tableView1 = (TableView)serializer.getNamespace().get("tableView1");
+        tableView2 = (TableView)serializer.getNamespace().get("tableView2");
+        treeView1 = (TreeView)serializer.getNamespace().get("treeView1");
+        treeView2 = (TreeView)serializer.getNamespace().get("treeView2");
 
         listButton1 = (ListButton)serializer.getNamespace().get("listButton1");
         listButton2 = (ListButton)serializer.getNamespace().get("listButton2");
@@ -110,21 +134,25 @@ public class Pivot694 implements Application {
             public void buttonPressed(Button button) {
                 System.out.println("Clearing selection from " + button.getName() + " at " + new Date());
 
-    //TODO: verify what to do on these ...
+// TODO: for release 2.1 ... maybe generalizing some test here
 //                pushButtons.clearSelection();
 //                toggleButtons.clearSelection();
 //                radioButtons.clearSelection();
 //                checkButtons.clearSelection();
 //                checkTriButtons.clearSelection();
-
+//
 //                calendarButton.clearSelection();
 //                spinner.clearSelection();
-
-                tableView.clearSelection();
-                treeView.clearSelection();
-
+//
 //              listButton1.clearSelection();
 //              listButton2.clearSelection();
+
+                listView1.clearSelection();
+                listView2.clearSelection();
+                tableView1.clearSelection();
+                tableView2.clearSelection();
+                treeView1.clearSelection();
+                treeView2.clearSelection();
 
             }
         });
@@ -135,27 +163,50 @@ public class Pivot694 implements Application {
             public void buttonPressed(Button button) {
                 System.out.println("Clearing data from " + button.getName() + " at " + new Date());
 
-// TODO: verify what to do on these ...
+// TODO: verify for release 2.1 if implement a method to empty components inside ...
 //                pushButtons.remove(button);
 //                toggleButtons.remove(button);
 //                radioButtons.remove(button);
 //                checkButtons.remove(button);
 //                checkTriButtons.remove(button);
 
-                calendarButton.clear();
-                spinner.clear();
+                // force clear of elements data ...
+                clearComponent(calendarButton1);  // TODO: doesn't seem to work on this component ...
+                clearComponent(calendarButton2);
+                clearComponent(spinner1);
+                clearComponent(spinner2);
+                clearComponent(listButton1);
+                clearComponent(listButton2);
 
-                tableView.clear();
-                treeView.clear();
+                clearComponent(listView1);
+                clearComponent(listView2);
+                clearComponent(tableView1);
+                clearComponent(tableView2);
+                clearComponent(treeView1);  // TODO: doesn't seem to work on this component ...
+                clearComponent(treeView2);
 
-                listButton1.clear();
-                listButton2.clear();
+// TODO: put (in bxml) the two tableView in a SplitPane, and see some strange moving mouse over  ...
+
+// TODO: add clear even to some type of buttons (all types in the first row displayed) ...
+
             }
         });
 
         System.out.println("initializeFields: end");
     }
 
+    protected static final void clearComponent(Component component) {
+        if (component == null)
+            throw new IllegalArgumentException();
+
+        component.clear();
+        component.repaint();
+
+        System.out.println("Component " + component + " with name \""
+            + component.getName() + "\""
+            + " cleared, and forced a repaint on it"
+        );
+    }
 
     public static void main(String[] args) {
         DesktopApplicationContext.main(Pivot694.class, args);
