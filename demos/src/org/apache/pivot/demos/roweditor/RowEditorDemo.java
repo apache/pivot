bf7f01829d3a5323109b8f8c2994f532aa7cdcfc
Sandro Martini <smartini@apache.org>
2011-11-11 13:38:42 +0000

    add RowEditorDemo Java class, to simplify run from sources in the right way (reading the resource file.). Note its Demo pages haven't been updated to use this (but continue to use the bxml file) because it show how to give arguments to bxml files.
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1200878 13f79535-47bb-0310-9956-ffa450edef68

---
+++ demos/src/org/apache/pivot/demos/roweditor/RowEditorDemo.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.demos.roweditor;
+
+import java.util.Locale;
+
+import org.apache.pivot.beans.BXMLSerializer;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Window;
+
+public class RowEditorDemo implements Application {
+    private Window window = null;
+
+    public static final String LANGUAGE_KEY = "language";
+    private static final String RESOURCE_NAME = RowEditorDemo.class.getName();
+
+    @Override
+    public void startup(Display display, Map<String, String> properties) throws Exception {
+        String language = properties.get(LANGUAGE_KEY);
+        Locale locale = (language == null) ? Locale.getDefault() : new Locale(language);
+        Resources resources = new Resources(RESOURCE_NAME, locale);
+        System.out.println("Loaded Resources from: " + resources.getBaseName() + ", for locale " + locale);
+
+        // Search for a font that can support the sample string
+        String title = (String)resources.get("title");
+        System.out.println("Title from Resources file is: \"" + title + "\"");
+
+        BXMLSerializer bxmlSerializer = new BXMLSerializer();
+        window = (Window)bxmlSerializer.readObject(RowEditorDemo.class.getResource("row_editor_demo.bxml"), resources);
+        window.open(display);
+    }
+
+    @Override
+    public boolean shutdown(boolean optional) {
+        if (window != null) {
+            window.close();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void suspend() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    public static void main(String[] args) {
+        DesktopApplicationContext.main(RowEditorDemo.class, args);
+    }
+
+}
