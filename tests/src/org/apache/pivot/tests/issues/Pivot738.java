da4b33c1429c843629236d36768d552fcb45706b
Sandro Martini <smartini@apache.org>
2012-05-07 13:29:07 +0000

    PIVOT-738, test application, now with paint debug flags enabled (in the Java source)
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1335012 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/src/org/apache/pivot/tests/issues/Pivot738.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.tests.issues;
+
+import org.apache.pivot.beans.BXMLSerializer;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Window;
+
+public class Pivot738 extends Application.Adapter {
+    private Window window = null;
+
+    public void startup(Display display, Map<String, String> properties) throws Exception {
+        BXMLSerializer bxmlSerializer = new BXMLSerializer();
+        window = (Window) bxmlSerializer.readObject(Pivot734.class, "pivot_738.bxml");
+        window.setMaximized(true);
+        window.setWidth(400);
+        window.setHeight(250);
+        window.open(display);
+    }
+
+    @Override
+    public boolean shutdown(boolean optional) {
+        if (window != null) {
+            window.close();
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // turn on debug drawing, to better see painting problems
+        // System.setProperty("org.apache.pivot.wtk.debugfocus", "true");  // debug focus
+        System.setProperty("org.apache.pivot.wtk.debugpaint", "true");  // debug paint
+
+        DesktopApplicationContext.main(new String[] { Pivot738.class.getName() });
+    }
+
+}
