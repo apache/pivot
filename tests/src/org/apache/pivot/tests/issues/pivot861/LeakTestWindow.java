c745606ed0aff4c0d672c5864a228c6a3d31e966
Sandro Martini <smartini@apache.org>
2012-07-26 17:44:13 +0000

    merge fixes from 2.0.x
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1366104 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/src/org/apache/pivot/tests/issues/pivot861/LeakTestWindow.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.tests.issues.pivot861;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+
+import org.apache.pivot.beans.BXML;
+import org.apache.pivot.beans.BXMLSerializer;
+import org.apache.pivot.beans.Bindable;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.serialization.SerializationException;
+import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.Action;
+import org.apache.pivot.wtk.ApplicationContext;
+import org.apache.pivot.wtk.Component;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.PushButton;
+import org.apache.pivot.wtk.Window;
+
+/**
+ * Simple test window which continuously presses a button which opens a dialog
+ * box and then closes it immediately.
+ *
+ * Watch the heap usage in a profiler and compare:
+ * - when TestDialog has an icon (specified in ok_dialog.bxml)
+ * - when TestDialog has no icon
+ * and see that heap usage will grow to almost the maximum when an icon is used
+ * in TestDialog.
+ *
+ * The icon contains an ImageListenerList which will retain a
+ * reference to every dialog created, thus preventing them being garbage collected.
+ */
+public class LeakTestWindow extends Window implements Bindable {
+
+    private static final String MARKUP_FILE = "/org/apache/pivot/tests/issues/pivot861/leak_test_window.bxml";
+
+    @BXML
+    PushButton button;
+
+    int dialogTest = 0;
+
+    public static LeakTestWindow create() throws IOException, SerializationException {
+        System.out.println("LeakTestWindow create()");
+        return (LeakTestWindow) new BXMLSerializer().readObject(LeakTestWindow.class, MARKUP_FILE);
+    }
+
+    @Override
+    public void initialize(Map<String, Object> arg0, URL arg1, Resources arg2) {
+        System.out.println("LeakTestWindow initialize(...)\n");
+
+        button.setAction(new Action() {
+            @Override
+            public void perform(Component component) {
+                dialogTest++;
+                System.out.println("Dialog test number " + dialogTest + " at " + new Date());
+                TestDialog dialog = TestDialog.create();
+
+                System.out.println("Opening the dialog");
+                dialog.open(LeakTestWindow.this);
+
+                // Close the dialog straight away
+                System.out.println("Closing the dialog");
+                dialog.close();
+
+                System.out.println("End of perform()\n");
+            }
+        });
+    }
+
+    @Override
+    public void open(Display display, Window owner) {
+        super.open(display, owner);
+
+        ApplicationContext.scheduleCallback(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    button.press();
+                }
+            }
+        }, 1000  // add a little delay (instead of 0) to show the TestDialog
+        );
+    }
+
+}
---
+++ tests/src/org/apache/pivot/tests/issues/pivot861/Pivot861.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.tests.issues.pivot861;
+
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+
+/**
+ * Test application to show the memory leak (the subject for this bug).
+ * For more info, look at LeakTestWindow.
+ */
+public class Pivot861 extends Application.Adapter {
+
+    public void startup(final Display display, Map<String, String> args) throws Exception {
+        System.out.println("Pivot861 startup(...)");
+        System.out.println("\n"
+            + "Attention: now the application will go in an infinite loop, to be able to see the memory leak.\n"
+            + "Note that probably you'll have to kill the application from outside (kill the Java process).\n"
+            + "\n"
+        );
+
+        // add some sleep to let users see the warning messages in console ...
+        Thread.sleep(2000);
+
+        LeakTestWindow window = LeakTestWindow.create();
+        window.open(display);
+    }
+
+    @Override
+    public boolean shutdown(boolean b) throws Exception {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        DesktopApplicationContext.main(Pivot861.class, args);
+    }
+
+}
