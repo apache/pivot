83a5f22ed867a38ad2947bd4b94665d391cbbf6d
Sandro Martini <smartini@apache.org>
2012-07-30 16:55:56 +0000

    PIVOT-859, first version of the test
    git-svn-id: https://svn.apache.org/repos/asf/pivot/branches/2.0.x@1367170 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/src/org/apache/pivot/tests/issues/pivot859/Pivot859.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.tests.issues.pivot859;
+
+import org.apache.pivot.beans.BXMLSerializer;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.wtk.Application;
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Window;
+
+/**
+ * Test application , to be run in multiple instances in the same HTML page.
+ */
+public class Pivot859 extends Application.Adapter {
+
+    private Window window = null;
+
+    public void startup(final Display display, Map<String, String> args) throws Exception {
+        System.out.println("Pivot859 startup(...)");
+
+        BXMLSerializer bxmlSerializer = new BXMLSerializer();
+        window = (Window) bxmlSerializer.readObject(Pivot859.class, "pivot_859.bxml");
+        window.open(display);
+    }
+
+    @Override
+    public boolean shutdown(boolean b) throws Exception {
+        if (window != null) {
+            window.close();
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        DesktopApplicationContext.main(Pivot859.class, args);
+    }
+
+}
---
+++ tests/src/org/apache/pivot/tests/issues/pivot859/README.txt
@@ -0,0 +1,12 @@
+//
+// README for Pivot859
+//
+
+This test must be run from a real browser, so before running the test you must generate Pivot jars, with the usual ant commands:
+
+ant package
+and package-tests
+
+Then open the html page here in a real browser.
+
+Note that this test multiple Applet instance inside the same HTML page, and all calls a resource via HTTP.
