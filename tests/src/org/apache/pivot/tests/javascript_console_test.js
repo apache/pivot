a35187cea78784f040d5d746d8d8af3731e70782
Sandro Martini <smartini@apache.org>
2012-08-07 16:32:57 +0000

    add a new Test Application (or small, still not completely working sample, but it's just to give the idea) with a minimal amount of Java code, and most of the code in JavaScript (but the JS interpreted by the JVM). This should show better how to interact with remote bxml files (for example generated or simply hosted at server-side) and do all the processing client side but in JS, more aligned to the HTML 5 way to do things.
    git-svn-id: https://svn.apache.org/repos/asf/pivot/branches/2.0.x@1370348 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/src/org/apache/pivot/tests/javascript_console_test.js
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+importPackage(java.lang);  // required to use System.out and System.err
+importPackage(org.apache.pivot.util);  // required to use Pivot Utility class Console
+importPackage(org.apache.pivot.wtk);   // required to use Pivot WTK classes
+
+
+function log(msg) {
+	if (msg == undefined || msg == null || typeof msg != "string")
+		return ;
+
+    System.out.println(msg);
+}
+
+function updateStatus(msg) {
+	if (msg == undefined || msg == null || typeof msg != "string")
+		return ;
+
+	if (msg.length < 50)
+		textStatus.text = msg;
+}
+
+function clearStatus() {
+	updateStatus("");
+}
+
+
+function runConsole() {
+	var text = textArea.text;
+	log("Console Text length = " + text.length());
+	if (text.length() < 1)
+		return ;
+	// else
+	var msg = "Run JS Code in Console";
+	log(msg);
+	updateStatus(msg + " ...");
+
+// TODO: continue here ...
+	;
+}
+
+//TODO: temp ...
+function openFrame(button) {
+    testFrame.open(window);
+    java.lang.System.out.println(label1.getText());
+    java.lang.System.out.println(label2.getText());
+}
