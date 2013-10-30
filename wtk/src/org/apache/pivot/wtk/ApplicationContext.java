c2de928acf96d1eb52d012bc0ee204526e315d6a
Roger Lee Whitcomb <rwhitcomb@apache.org>
2013-10-30 18:12:43 +0000

    PIVOT-924: Change scope of ApplicationContext.handleUncaughtException to public so it can be called from outside places (re: BXMLSerializer), meaning it is the ONE place where all uncaught exceptions are finally processed.
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1537222 13f79535-47bb-0310-9956-ffa450edef68

--- wtk/src/org/apache/pivot/wtk/ApplicationContext.java
+++ wtk/src/org/apache/pivot/wtk/ApplicationContext.java
@@ -1989,7 +1989,7 @@ public abstract class ApplicationContext {
         return cursor;
     }
 
-    protected static void handleUncaughtException(Exception exception) {
+    public static void handleUncaughtException(Exception exception) {
         int n = 0;
         for (Application application : applications) {
             if (application instanceof Application.UncaughtExceptionHandler) {
