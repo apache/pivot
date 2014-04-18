a300486ace28fe8cfb98ec997ef64e26bfa0d062
Sandro Martini <smartini@apache.org>
2014-04-18 10:36:53 +0000

    PIVOT-942, merge from 2.0.x
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1588436 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wtk/src/org/apache/pivot/wtk/skin/TabPaneSkin.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.wtk.skin;
+
+import org.apache.pivot.wtk.BoxPane;
+import org.apache.pivot.wtk.TabPane;
+
+/**
+ * Tab pane skin. Note that this class is abstract but only because in layout
+ * method there are many things already defined that uses the original skin
+ * implementation (TerraTabPaneSkin).
+ */
+public abstract class TabPaneSkin extends ContainerSkin implements TabPane.Skin {
+
+    protected BoxPane tabButtonBoxPane = new BoxPane();
+
+    @Override
+    public boolean isVisible(int index) {
+        return tabButtonBoxPane.get(index).isVisible();
+    }
+
+    @Override
+    public void setVisible(int index, boolean value) {
+        tabButtonBoxPane.get(index).setVisible(value);
+    }
+
+}
