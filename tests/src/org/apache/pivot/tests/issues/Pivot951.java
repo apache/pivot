05509bffedc6b35dccf9dfea9d351853a30b46f8
Sandro Martini <smartini@apache.org>
2014-06-13 16:23:09 +0000

    PIVOT-951: add Apache license, and update source to our coding standards
    git-svn-id: https://svn.apache.org/repos/asf/pivot/branches/2.0.x@1602467 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/org/apache/pivot/tests/issues/Pivot951.java
+++ tests/src/org/apache/pivot/tests/issues/Pivot951.java
@@ -1,4 +1,20 @@
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.tests.issues;
 
 import org.apache.pivot.collections.Map;
 import org.apache.pivot.wtk.Application;
@@ -9,33 +25,34 @@ import org.apache.pivot.wtk.DesktopApplicationContext;
 import org.apache.pivot.wtk.Display;
 import org.apache.pivot.wtk.Mouse.Button;
 import org.apache.pivot.wtk.Window;
- 
-public class Pivot951 implements Application {
+
+public class Pivot951 extends Application.Adapter {
     private Window window = null;
 
     @Override
     public void startup(Display display, Map<String, String> properties) {
         window = new Window();
- 
+
         Border brd = new Border();
         brd.getComponentMouseButtonListeners().add(new ComponentMouseButtonListener.Adapter() {
-			@Override
-			public boolean mouseClick(Component component, Button button, int x, int y, int count) {
-				if (count == 1)
-					System.out.println("Click!");
-				else
-					System.out.println("Double Click!");
-				return true;
-			}
-		});
-
+            @Override
+            public boolean mouseClick(Component component, Button button, int x, int y, int count) {
+                if (count == 1) {
+                    System.out.println("Click!");
+                } else {
+                    System.out.println("Double Click!");
+                }
+                return true;
+            }
+        });
+        
         window.setContent(brd);
         window.setTitle("Pivot951: Cannot click twice");
         window.setMaximized(true);
- 
+
         window.open(display);
     }
- 
+
     @Override
     public boolean shutdown(boolean optional) {
         if (window != null) {
@@ -43,12 +60,9 @@ public class Pivot951 implements Application {
         }
         return false;
     }
- 
-    @Override public void suspend() { }
-    @Override public void resume() { }
-    
+
     public static void main(String[] args) {
         DesktopApplicationContext.main(Pivot951.class, args);
     }
-}
 
+}
