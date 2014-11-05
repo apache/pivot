a05ae6d117e78a811a0945f6111a3c219ca55521
Sandro Martini <smartini@apache.org>
2014-11-05 08:50:46 +0000

    PIVOT-949, the test application
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1636826 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/src/org/apache/pivot/tests/utilities_script.js
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+importPackage(java.lang);  // required to use System.out and System.err
+importPackage(org.apache.pivot.collections);  // required to use Pivot class ArrayList and other collections
+importPackage(org.apache.pivot.util);  // required to use Pivot Utility class Console
+importPackage(org.apache.pivot.wtk);   // required to use Pivot WTK classes
+
+
+function log(msg) {
+	if (msg == undefined || msg == null || typeof msg != "string")
+		return ;
+
+    System.out.println(msg);
+}
+
+function logObject(msg) {
+	if (msg == undefined || msg == null)
+		return ;
+
+	application.logObject(msg);
+}
+
