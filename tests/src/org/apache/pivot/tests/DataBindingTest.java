8bd604ed5d263621bf44daafa02041b4c446d592
Roger Lee Whitcomb <rwhitcomb@apache.org>
2016-02-09 23:27:16 +0000

    Clean up the DataBindingTest so that it actually works now.  This actually only involved simplifying the renderers to only implement the "toString(Object)" methods.
    But, make the values more interesting, and add explanatory stuff to the GUI
    so it is more understandable what is actually happening.
    
    This is a merge of revision 1729480 from trunk to branches/2.0.x with the
    HyperlinkButton changed to just a Label, and the Java 7 changes reverted.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/pivot/branches/2.0.x@1729501 13f79535-47bb-0310-9956-ffa450edef68

--- tests/src/org/apache/pivot/tests/DataBindingTest.java
+++ tests/src/org/apache/pivot/tests/DataBindingTest.java
@@ -26,6 +26,7 @@ import org.apache.pivot.wtk.Application;
 import org.apache.pivot.wtk.Button;
 import org.apache.pivot.wtk.DesktopApplicationContext;
 import org.apache.pivot.wtk.Display;
+import org.apache.pivot.wtk.Label;
 import org.apache.pivot.wtk.ListView;
 import org.apache.pivot.wtk.Spinner;
 import org.apache.pivot.wtk.Window;
@@ -36,33 +37,12 @@ import org.apache.pivot.wtk.content.SpinnerItemRenderer;
 public class DataBindingTest extends Application.Adapter {
     public static class TestListButtonDataRenderer extends ListButtonDataRenderer {
         @Override
-        public void render(final Object data, Button button, boolean highlighted) {
-            Object dataLoaded = data;
-            if (dataLoaded != null) {
-                dataLoaded = JSON.get(data, "text");
-            }
-
-            super.render(dataLoaded, button, highlighted);
-        }
-
-        @Override
-        public String toString(Object data) {
-            return JSON.get(data, "text");
+        public String toString(Object item) {
+            return JSON.get(item, "text");
         }
     }
 
     public static class TestListViewItemRenderer extends ListViewItemRenderer {
-        @Override
-        public void render(final Object item, int index, final ListView listView, boolean selected,
-            boolean checked, boolean highlighted, boolean disabled) {
-            Object itemLoaded = item;
-            if (itemLoaded != null) {
-                itemLoaded = JSON.get(item, "text");
-            }
-
-            super.render(itemLoaded, index, listView, selected, checked, highlighted, disabled);
-        }
-
         @Override
         public String toString(Object item) {
             return JSON.get(item, "text");
@@ -71,29 +51,20 @@ public class DataBindingTest extends Application.Adapter {
 
     public static class TestSpinnerItemRenderer extends SpinnerItemRenderer {
         @Override
-        public void render(final Object item, final Spinner spinner) {
-            Object itemLoaded = item;
-            if (itemLoaded != null) {
-                itemLoaded = JSON.get(item, "text");
-            }
-
-            super.render(itemLoaded, spinner);
-        }
-
-        @Override
         public String toString(Object item) {
             return JSON.get(item, "text");
         }
     }
 
-    public static class TestBindMapping implements ListView.ItemBindMapping, Spinner.ItemBindMapping {
+    public static class TestBindMapping
+        implements ListView.ItemBindMapping, Spinner.ItemBindMapping {
         @Override
-        @SuppressWarnings("unchecked")
         public int indexOf(List<?> list, Object value) {
             int i = 0;
             int n = list.getLength();
             while (i < n) {
-                Map<String, ?> map = (Map<String, ?>)list.get(i);
+                @SuppressWarnings("unchecked")
+                Map<String, ?> map = (Map<String, ?>) list.get(i);
                 if (map.get("id").equals(value)) {
                     break;
                 }
@@ -109,9 +80,9 @@ public class DataBindingTest extends Application.Adapter {
         }
 
         @Override
-        @SuppressWarnings("unchecked")
         public Object get(List<?> list, int index) {
-            Map<String, ?> map = (Map<String, ?>)list.get(index);
+            @SuppressWarnings("unchecked")
+            Map<String, ?> map = (Map<String, ?>) list.get(index);
             return map.get("id");
         }
     }
@@ -121,7 +92,7 @@ public class DataBindingTest extends Application.Adapter {
     @Override
     public void startup(Display display, Map<String, String> properties) throws Exception {
         BXMLSerializer bxmlSerializer = new BXMLSerializer();
-        window = (Window)bxmlSerializer.readObject(DataBindingTest.class, "data_binding_test.bxml");
+        window = (Window) bxmlSerializer.readObject(DataBindingTest.class, "data_binding_test.bxml");
         window.open(display);
 
         HashMap<String, Object> context = new HashMap<String, Object>();
@@ -134,7 +105,8 @@ public class DataBindingTest extends Application.Adapter {
         context = new HashMap<String, Object>();
         window.getContent().store(context);
 
-        System.out.println(JSONSerializer.toString(context));
+        Label textLabel = (Label)(bxmlSerializer.getNamespace().get("bindingDataText"));
+        textLabel.setText(JSONSerializer.toString(context));
     }
 
     @Override
