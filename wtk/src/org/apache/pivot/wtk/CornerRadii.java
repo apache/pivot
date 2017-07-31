6a6cd67e7119cebe097b639707b42393fe39948e
Roger Lee Whitcomb <rwhitcomb@apache.org>
2017-07-31 23:42:26 +0000

    Simplified and cleaned up CornerRadii as with other "wtk" utility classes: * Use common parameter and value checking methods (such as Utils.checkNull). * Allow "decode" method to use JSON arrays (as well as maps). * Take advantage of new Dictionary default methods (Java 8). * Use simple class name for "toString()".
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1803598 13f79535-47bb-0310-9956-ffa450edef68

--- wtk/src/org/apache/pivot/wtk/CornerRadii.java
+++ wtk/src/org/apache/pivot/wtk/CornerRadii.java
@@ -19,8 +19,10 @@ package org.apache.pivot.wtk;
 import java.io.Serializable;
 
 import org.apache.pivot.collections.Dictionary;
+import org.apache.pivot.collections.List;
 import org.apache.pivot.json.JSONSerializer;
 import org.apache.pivot.serialization.SerializationException;
+import org.apache.pivot.util.Utils;
 
 /**
  * Class representing the corner radii of a rectangular object.
@@ -47,34 +49,11 @@ public final class CornerRadii implements Serializable {
         this(radius, radius, radius, radius);
     }
 
-    public CornerRadii(CornerRadii cornerRadii) {
-        if (cornerRadii == null) {
-            throw new IllegalArgumentException("cornerRadii is null.");
-        }
-
-        if (cornerRadii.topLeft < 0) {
-            throw new IllegalArgumentException("cornerRadii.topLeft is negative.");
-        }
-
-        if (cornerRadii.topRight < 0) {
-            throw new IllegalArgumentException("cornerRadii.topRight is negative.");
-        }
-
-        if (cornerRadii.bottomLeft < 0) {
-            throw new IllegalArgumentException("cornerRadii.bottomLeft is negative.");
-        }
-
-        if (cornerRadii.bottomRight < 0) {
-            throw new IllegalArgumentException("cornerRadii.bottomRight is negative.");
-        }
-
-        this.topLeft = cornerRadii.topLeft;
-        this.topRight = cornerRadii.topRight;
-        this.bottomLeft = cornerRadii.bottomLeft;
-        this.bottomRight = cornerRadii.bottomRight;
+    private void check(CornerRadii radii) {
+        check(radii.topLeft, radii.topRight, radii.bottomLeft, radii.bottomRight);
     }
 
-    public CornerRadii(int topLeft, int topRight, int bottomLeft, int bottomRight) {
+    private void check(int topLeft, int topRight, int bottomLeft, int bottomRight) {
         if (topLeft < 0) {
             throw new IllegalArgumentException("topLeft is negative.");
         }
@@ -90,6 +69,22 @@ public final class CornerRadii implements Serializable {
         if (bottomRight < 0) {
             throw new IllegalArgumentException("bottomRight is negative.");
         }
+
+    }
+
+    public CornerRadii(CornerRadii cornerRadii) {
+        Utils.checkNull(cornerRadii, "cornerRadii");
+
+        check(cornerRadii);
+
+        this.topLeft = cornerRadii.topLeft;
+        this.topRight = cornerRadii.topRight;
+        this.bottomLeft = cornerRadii.bottomLeft;
+        this.bottomRight = cornerRadii.bottomRight;
+    }
+
+    public CornerRadii(int topLeft, int topRight, int bottomLeft, int bottomRight) {
+        check(topLeft, topRight, bottomLeft, bottomRight);
 
         this.topLeft = topLeft;
         this.topRight = topRight;
@@ -107,49 +102,14 @@ public final class CornerRadii implements Serializable {
      * treated as zero.
      */
     public CornerRadii(Dictionary<String, ?> cornerRadii) {
-        if (cornerRadii == null) {
-            throw new IllegalArgumentException("cornerRadii is null.");
-        }
+        Utils.checkNull(cornerRadii, "cornerRadii");
 
-        if (cornerRadii.containsKey(TOP_LEFT_KEY)) {
-            topLeft = ((Integer) cornerRadii.get(TOP_LEFT_KEY)).intValue();
+        topLeft = cornerRadii.getInt(TOP_LEFT_KEY, 0);
+        topRight = cornerRadii.getInt(TOP_RIGHT_KEY, 0);
+        bottomLeft = cornerRadii.getInt(BOTTOM_LEFT_KEY, 0);
+        bottomRight = cornerRadii.getInt(BOTTOM_RIGHT_KEY, 0);
 
-            if (topLeft < 0) {
-                throw new IllegalArgumentException("\"topLeft\" is negative.");
-            }
-        } else {
-            topLeft = 0;
-        }
-
-        if (cornerRadii.containsKey(TOP_RIGHT_KEY)) {
-            topRight = ((Integer) cornerRadii.get(TOP_RIGHT_KEY)).intValue();
-
-            if (topRight < 0) {
-                throw new IllegalArgumentException("\"topRight\" is negative.");
-            }
-        } else {
-            topRight = 0;
-        }
-
-        if (cornerRadii.containsKey(BOTTOM_LEFT_KEY)) {
-            bottomLeft = ((Integer) cornerRadii.get(BOTTOM_LEFT_KEY)).intValue();
-
-            if (bottomLeft < 0) {
-                throw new IllegalArgumentException("\"bottomLeft\" is negative.");
-            }
-        } else {
-            bottomLeft = 0;
-        }
-
-        if (cornerRadii.containsKey(BOTTOM_RIGHT_KEY)) {
-            bottomRight = ((Integer) cornerRadii.get(BOTTOM_RIGHT_KEY)).intValue();
-
-            if (bottomRight < 0) {
-                throw new IllegalArgumentException("\"bottomRight\" is negative.");
-            }
-        } else {
-            bottomRight = 0;
-        }
+        check(this);
     }
 
     @Override
@@ -178,19 +138,44 @@ public final class CornerRadii implements Serializable {
 
     @Override
     public String toString() {
-        return getClass().getName() + " [" + topLeft + ", " + topRight + bottomLeft + ", "
-            + bottomRight + "]";
+        return getClass().getSimpleName() + " [" + topLeft + "," + topRight + "; "
+            + bottomLeft + "," + bottomRight + "]";
     }
 
+    /**
+     * Convert a string into corner radii.
+     * <p> If the string value is a JSON map, then parse the map
+     * and construct using the {@link #CornerRadii(Dictionary)} method.
+     * <p> If the string value is a JSON list, then parse the list
+     * and construct using the first four values as top left, top right,
+     * bottom left, and bottom right respectively, using the
+     * {@link #CornerRadii(int, int, int, int)} constructor.
+     * <p> Otherwise the string should be a single integer value
+     * that will be used to construct the radii using the {@link #CornerRadii(int)}
+     * constructor.
+     *
+     * @param value The string value to decode into new corner radii.
+     * @return The decoded corner radii.
+     * @throws IllegalArgumentException if the value is {@code null} or
+     * if the string starts with <code>"{"</code> but it cannot be parsed as
+     * a JSON map, or if it starts with <code>"["</code> but cannot be parsed
+     * as a JSON list.
+     */
     public static CornerRadii decode(String value) {
-        if (value == null) {
-            throw new IllegalArgumentException();
-        }
+        Utils.checkNull(value, "value");
 
         CornerRadii cornerRadii;
         if (value.startsWith("{")) {
             try {
                 cornerRadii = new CornerRadii(JSONSerializer.parseMap(value));
+            } catch (SerializationException exception) {
+                throw new IllegalArgumentException(exception);
+            }
+        } else if (value.startsWith("[")) {
+            try {
+                @SuppressWarnings("unchecked")
+                List<Integer> values = (List<Integer>)JSONSerializer.parseList(value);
+                cornerRadii = new CornerRadii(values.get(0), values.get(1), values.get(2), values.get(3));
             } catch (SerializationException exception) {
                 throw new IllegalArgumentException(exception);
             }
