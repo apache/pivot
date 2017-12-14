c0561fb1aaec4882e515beaa5cca7b5a3ca0c03d
Roger Lee Whitcomb <rwhitcomb@apache.org>
2017-12-14 17:02:25 +0000

    PIVOT-1017:  Move the "Ruler" and associated classes (Skin and Listener) into the mainline code for general use.  Remove it from the "tutorials" area (both copies) and update the references to use the general one. Made one change to the code:  update the drawing to make the 1/2 mark slightly longer than the others to look more like a real Ruler.
    git-svn-id: https://svn.apache.org/repos/asf/pivot/trunk@1818168 13f79535-47bb-0310-9956-ffa450edef68

---
+++ wtk/src/org/apache/pivot/wtk/Ruler.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.pivot.wtk;
+
+import org.apache.pivot.util.ListenerList;
+import org.apache.pivot.util.Utils;
+import org.apache.pivot.wtk.Component;
+import org.apache.pivot.wtk.Orientation;
+
+/**
+ * A viewport header that displays a ruler along either the top or left side.
+ */
+public class Ruler extends Component {
+
+    private static class RulerListenerList extends ListenerList<RulerListener> implements
+        RulerListener {
+        @Override
+        public void orientationChanged(Ruler ruler) {
+            forEach(listener -> listener.orientationChanged(ruler));
+        }
+    }
+
+    private Orientation orientation;
+
+    private RulerListenerList rulerListeners = new RulerListenerList();
+
+    public Ruler() {
+        setSkin(new RulerSkin());
+    }
+
+    public Orientation getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(Orientation orientation) {
+        Utils.checkNull(orientation, "orientation");
+
+        if (this.orientation != orientation) {
+            this.orientation = orientation;
+            rulerListeners.orientationChanged(this);
+        }
+    }
+
+    public ListenerList<RulerListener> getRulerListeners() {
+        return rulerListeners;
+    }
+}
